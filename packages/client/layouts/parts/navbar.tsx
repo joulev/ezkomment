23adec0ec1be6aaa350416dca72831b91e8163ff
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-19 01:38:27 +0800

    add a vercel-like secondary navbar
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/layouts/parts/navbar.tsx
+++ packages/client/layouts/parts/navbar.tsx
@@ -1,12 +1,10 @@
+import clsx from "clsx";
 import Image from "next/image";
-import type { FC } from "react";
+import type { FC, MouseEventHandler } from "react";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import A from "@client/components/anchor";
-
-const navbarBtnStyle =
-  "text-gray-500 rounded p-1 transition hover:bg-indigo-100 hover:text-indigo-500";
 
 const BreadCrumbSlash: FC = () => (
   <svg width={12} height={32}>
@@ -14,30 +12,73 @@ const BreadCrumbSlash: FC = () => (
   </svg>
 );
 
+type TopNavbarButtonProps = { href?: string; onClick?: MouseEventHandler<HTMLButtonElement> };
+const TopNavbarButton: FC<TopNavbarButtonProps> = ({ href, onClick, children }) => {
+  const navbarBtnStyle =
+    "text-gray-500 rounded p-1 transition hover:bg-indigo-100 hover:text-indigo-500";
+  return href ? (
+    <A href="/app" notStyled className={navbarBtnStyle}>
+      {children}
+    </A>
+  ) : (
+    <button className={navbarBtnStyle} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+type MainNavbarButtonProps = { href: string; active?: boolean };
+const MainNavbarButton: FC<MainNavbarButtonProps> = ({ href, active, children }) => {
+  return (
+    <A
+      href={href}
+      notStyled
+      className={clsx(
+        "py-1 px-2 rounded transition hover:bg-gray-200 hover:text-gray-900",
+        active ? "text-gray-900" : "text-gray-500",
+        active &&
+          "relative after:absolute after:-bottom-2 after:inset-x-2 after:h-0 after:border-b after:border-gray-900"
+      )}
+    >
+      {children}
+    </A>
+  );
+};
+
 const Navbar: FC = () => (
   <header className="bg-white border-b border-gray-300">
-    <nav className="container flex flex-row gap-6 py-3 items-center justify-between">
-      <div className="hidden sm:flex flex-row gap-3 items-center">
-        <A href="/app" notStyled className="w-8 h-8 relative">
-          <Image src="/logo.svg" alt="ezkomment" layout="fill" />
+    <div className="container">
+      <nav className="flex flex-row gap-6 py-3 items-center justify-between">
+        <div className="hidden sm:flex flex-row gap-3 items-center">
+          <A href="/app" notStyled className="w-8 h-8 relative">
+            <Image src="/logo.svg" alt="ezkomment" layout="fill" />
+          </A>
+          <BreadCrumbSlash />
+          <div className="text-xl">Overview</div>
+        </div>
+        <div className="hidden sm:block flex-grow" />
+        <TopNavbarButton href="/app">
+          <HomeOutlinedIcon />
+        </TopNavbarButton>
+        <TopNavbarButton>
+          <NotificationsOutlinedIcon />
+        </TopNavbarButton>
+        <A href="/app/account" className="h-8 w-8 shrink-0 relative">
+          <Image src="/default-photo.svg" alt="" layout="fill" />
         </A>
-        <BreadCrumbSlash />
-        <div className="text-xl">Overview</div>
-      </div>
-      <div className="hidden sm:block flex-grow" />
-      <A href="/app" notStyled className={navbarBtnStyle}>
-        <HomeOutlinedIcon />
-      </A>
-      <button className={navbarBtnStyle}>
-        <NotificationsOutlinedIcon />
-      </button>
-      <A href="/app/account" className="h-8 w-8 shrink-0 relative">
-        <Image src="/default-photo.svg" alt="" layout="fill" />
-      </A>
-      <button className={navbarBtnStyle}>
-        <LogoutOutlinedIcon />
-      </button>
-    </nav>
+        <TopNavbarButton>
+          <LogoutOutlinedIcon />
+        </TopNavbarButton>
+      </nav>
+      <nav className="flex flex-row gap-2 -ml-2 pb-2">
+        <MainNavbarButton href="/" active>
+          All pages
+        </MainNavbarButton>
+        <MainNavbarButton href="/">Pending comments</MainNavbarButton>
+        <MainNavbarButton href="/">Customise</MainNavbarButton>
+        <MainNavbarButton href="/">Settings</MainNavbarButton>
+      </nav>
+    </div>
   </header>
 );
 
