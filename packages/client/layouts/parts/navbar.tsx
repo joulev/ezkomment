f3ad316e1366523f91fbe011ffc473ba1453f119
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-18 22:45:05 +0800

    add a custom document for client's Next.js
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/layouts/parts/navbar.tsx
+++ packages/client/layouts/parts/navbar.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
 
-const Navbar: FC = () => <div>Navbar header</div>;
+const Navbar: FC = () => <nav className="bg-white border-b border-gray-400 h-16"></nav>;
 
 export default Navbar;
---
+++ packages/client/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import type { NextPage } from "next";
+
+const Document: NextPage = () => (
+  <Html lang="en">
+    <Head />
+    <body className="bg-gray-100 text-gray-900">
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
