4f1093a259b64e0686975391dd04d1bdf892aea7
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-18 22:39:32 +0800

    use Bootstrap's breakpoints
    Tailwind breakpoints set is one of the few things I don't like about it...
    
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["**/*.tsx"],
   theme: {
+    // Screen breakpoints from Bootstrap, https://getbootstrap.com/docs/5.1/layout/breakpoints
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1400px",
+    },
     extend: {},
   },
   plugins: [],
