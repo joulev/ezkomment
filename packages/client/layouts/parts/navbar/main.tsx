b233d3278658096a7400384c4f3220b075b01ad8
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-20 20:48:46 +0800

    make main navbar active line thicker
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/layouts/parts/navbar/main.tsx
+++ packages/client/layouts/parts/navbar/main.tsx
@@ -12,7 +12,7 @@ const MainNavButton: FC<MainNavButtonProps> = ({ href, active, className, childr
         "py-1 px-2 rounded transition hover:bg-gray-200 hover:text-gray-900 whitespace-nowrap",
         active ? "text-gray-900" : "text-gray-500",
         active &&
-          "relative after:absolute after:-bottom-2 after:inset-x-2 after:h-0 after:border-b after:border-gray-900",
+          "relative after:absolute after:-bottom-[9px] after:inset-x-2 after:h-0 after:border-b-2 after:border-gray-900",
         className
       )}
     >
@@ -22,26 +22,28 @@ const MainNavButton: FC<MainNavButtonProps> = ({ href, active, className, childr
 };
 
 const MainNav: FC = () => (
-  <nav className="max-w-full overflow-scroll no-scrollbar sm:overflow-visible">
-    <div className="flex flex-row gap-2 -ml-2 pb-2">
-      <MainNavButton href="/" active>
-        All pages
-      </MainNavButton>
-      <MainNavButton href="/" className="sm:hidden">
-        Pending
-      </MainNavButton>
-      <MainNavButton href="/" className="hidden sm:block">
-        Pending comments
-      </MainNavButton>
-      <MainNavButton href="/" className="sm:hidden">
-        Customise
-      </MainNavButton>
-      <MainNavButton href="/" className="hidden sm:block">
-        Customise display
-      </MainNavButton>
-      <MainNavButton href="/">Settings</MainNavButton>
-    </div>
-  </nav>
+  <div className="container">
+    <nav className="max-w-full">
+      <div className="flex flex-row gap-2 -ml-2 pb-2">
+        <MainNavButton href="/" active>
+          All pages
+        </MainNavButton>
+        <MainNavButton href="/" className="sm:hidden">
+          Pending
+        </MainNavButton>
+        <MainNavButton href="/" className="hidden sm:block">
+          Pending comments
+        </MainNavButton>
+        <MainNavButton href="/" className="sm:hidden">
+          Customise
+        </MainNavButton>
+        <MainNavButton href="/" className="hidden sm:block">
+          Customise display
+        </MainNavButton>
+        <MainNavButton href="/">Settings</MainNavButton>
+      </div>
+    </nav>
+  </div>
 );
 
 export default MainNav;
