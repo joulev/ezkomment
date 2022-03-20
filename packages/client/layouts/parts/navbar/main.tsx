88bfd9d9ccfa4131a894862da43dc6c717cce4cb
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-20 20:53:08 +0800

    remove padding and background effects in mainnav elements
    We will have a different div dedicated for the background effect.
    
    This is super Vercel-ish but I love that design. Please don't copyright-strike me.
    
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/layouts/parts/navbar/main.tsx
+++ packages/client/layouts/parts/navbar/main.tsx
@@ -9,10 +9,10 @@ const MainNavButton: FC<MainNavButtonProps> = ({ href, active, className, childr
       href={href}
       notStyled
       className={clsx(
-        "py-1 px-2 rounded transition hover:bg-gray-200 hover:text-gray-900 whitespace-nowrap",
+        "transition hover:text-gray-900 whitespace-nowrap",
         active ? "text-gray-900" : "text-gray-500",
         active &&
-          "relative after:absolute after:-bottom-[9px] after:inset-x-2 after:h-0 after:border-b-2 after:border-gray-900",
+          "relative after:absolute after:-bottom-[9px] after:inset-x-0 after:h-0 after:border-b-2 after:border-gray-900",
         className
       )}
     >
@@ -24,7 +24,7 @@ const MainNavButton: FC<MainNavButtonProps> = ({ href, active, className, childr
 const MainNav: FC = () => (
   <div className="container">
     <nav className="max-w-full">
-      <div className="flex flex-row gap-2 -ml-2 pb-2">
+      <div className="flex flex-row gap-6 pb-2">
         <MainNavButton href="/" active>
           All pages
         </MainNavButton>
