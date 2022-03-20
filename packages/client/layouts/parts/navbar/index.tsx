116772572256fab57ef777df5acf2069de5fad5d
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-20 21:30:25 +0800

    make the main nav sticky-top
    If I set `sticky` to <nav> in <MainNav> directly, sticky will not work. Think for yourself why it works that way and why top is -16
    
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/layouts/parts/navbar/index.tsx
+++ packages/client/layouts/parts/navbar/index.tsx
@@ -3,7 +3,7 @@ import MainNav from "./main";
 import TopNav from "./top";
 
 const Navbar: FC = () => (
-  <header className="bg-white border-b border-gray-300">
+  <header className="bg-white border-b border-gray-300 sticky -top-16">
     <TopNav />
     <MainNav />
   </header>
