b52f1fc43b077568066ace9eac1411a66fe45442
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-20 17:53:05 +0800

    refactor topnav: add the navigation panel for mobile screen
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/layouts/parts/navbar/top.tsx
+++ packages/client/layouts/parts/navbar/top.tsx
@@ -1,10 +1,50 @@
+import clsx from "clsx";
 import Image from "next/image";
+import { useState } from "react";
 import type { FC, MouseEventHandler } from "react";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import DensityMediumOutlinedIcon from "@mui/icons-material/DensityMediumOutlined";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import A from "@client/components/anchor";
+
+type LinkOrButtonProps =
+  | { href: string; onClick?: never }
+  | { href?: never; onClick: MouseEventHandler<HTMLButtonElement> };
+type TopNavItemProps = { icon: typeof HomeOutlinedIcon } & LinkOrButtonProps;
+
+const TopNavButton: FC<TopNavItemProps> = ({ href, onClick, icon: Icon }) => {
+  const classes = "text-gray-500 rounded p-1 transition hover:bg-indigo-100 hover:text-indigo-500";
+  return href ? (
+    <A href="/app" notStyled className={classes}>
+      <Icon />
+    </A>
+  ) : (
+    <button className={classes} onClick={onClick}>
+      <Icon />
+    </button>
+  );
+};
+
+const TopNavExpandedItem: FC<TopNavItemProps> = ({ href, onClick, icon: Icon, children }) => {
+  const classes = clsx(
+    "py-3 border-t border-gray-300 text-left flex flex-row items-center gap-3",
+    "transition text-gray-500 hover:text-gray-900 hover:font-medium"
+  );
+  return href ? (
+    <A href={href} notStyled className={classes}>
+      <Icon className="mx-1" />
+      <div>{children}</div>
+    </A>
+  ) : (
+    <button onClick={onClick} className={classes}>
+      <Icon className="mx-1" />
+      <div>{children}</div>
+    </button>
+  );
+};
 
 const BreadCrumbSlash: FC = () => (
   <svg width={12} height={32}>
@@ -12,49 +52,51 @@ const BreadCrumbSlash: FC = () => (
   </svg>
 );
 
-type TopNavButtonProps = {
-  href?: string;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
-  icon: FC;
-};
-const TopNavButton: FC<TopNavButtonProps> = ({ href, onClick, icon: Icon }) => {
-  const navbarBtnStyle =
-    "text-gray-500 rounded p-1 transition hover:bg-indigo-100 hover:text-indigo-500";
-  return href ? (
-    <A href="/app" notStyled className={navbarBtnStyle}>
-      <Icon />
-    </A>
-  ) : (
-    <button className={navbarBtnStyle} onClick={onClick}>
-      <Icon />
-    </button>
+const TopNav: FC = () => {
+  const [expanded, setExpanded] = useState(false);
+  const handleLogout: MouseEventHandler<HTMLButtonElement> = () => console.log("log out");
+  const handleNotif: MouseEventHandler<HTMLButtonElement> = () => console.log("notif");
+  return (
+    <>
+      <nav className="hidden sm:flex flex-row gap-6 py-3 items-center justify-between">
+        <div className="flex flex-row gap-3 items-center">
+          <A href="/app" notStyled className="w-8 h-8 relative">
+            <Image src="/logo.svg" alt="ezkomment" layout="fill" />
+          </A>
+          <BreadCrumbSlash />
+          <div className="font-semibold">Overview</div>
+        </div>
+        <div className="flex-grow" />
+        <TopNavButton href="/app" icon={HomeOutlinedIcon} />
+        <TopNavButton onClick={handleNotif} icon={NotificationsOutlinedIcon} />
+        <A href="/app/account" className="h-8 w-8 shrink-0 relative">
+          <Image src="/default-photo.svg" alt="" layout="fill" />
+        </A>
+        <TopNavButton onClick={handleLogout} icon={LogoutOutlinedIcon} />
+      </nav>
+      <div className={clsx("sm:hidden", expanded && "fixed inset-0 bg-white z-10 px-6")}>
+        <nav className="flex flex-row py-3 items-center justify-between">
+          <TopNavButton
+            icon={expanded ? CloseOutlinedIcon : DensityMediumOutlinedIcon}
+            onClick={() => setExpanded(!expanded)}
+          />
+          <div className="font-semibold">Overview</div>
+          <TopNavButton onClick={handleNotif} icon={NotificationsOutlinedIcon} />
+        </nav>
+        <nav className={clsx(expanded || "hidden", "flex flex-col")}>
+          <TopNavExpandedItem icon={HomeOutlinedIcon} href="/app">
+            Dashboard
+          </TopNavExpandedItem>
+          <TopNavExpandedItem icon={SettingsOutlinedIcon} href="/app/account">
+            Account settings
+          </TopNavExpandedItem>
+          <TopNavExpandedItem icon={LogoutOutlinedIcon} onClick={handleLogout}>
+            Log out
+          </TopNavExpandedItem>
+        </nav>
+      </div>
+    </>
   );
 };
-
-const TopNav: FC = () => (
-  <>
-    <nav className="hidden sm:flex flex-row gap-6 py-3 items-center justify-between">
-      <div className="flex flex-row gap-3 items-center">
-        <A href="/app" notStyled className="w-8 h-8 relative">
-          <Image src="/logo.svg" alt="ezkomment" layout="fill" />
-        </A>
-        <BreadCrumbSlash />
-        <div className="font-semibold">Overview</div>
-      </div>
-      <div className="flex-grow" />
-      <TopNavButton href="/app" icon={HomeOutlinedIcon} />
-      <TopNavButton icon={NotificationsOutlinedIcon} />
-      <A href="/app/account" className="h-8 w-8 shrink-0 relative">
-        <Image src="/default-photo.svg" alt="" layout="fill" />
-      </A>
-      <TopNavButton icon={LogoutOutlinedIcon} />
-    </nav>
-    <nav className="sm:hidden flex flex-row py-3 items-center justify-between">
-      <TopNavButton icon={DensityMediumOutlinedIcon} />
-      <div className="font-semibold">Overview</div>
-      <TopNavButton icon={NotificationsOutlinedIcon} />
-    </nav>
-  </>
-);
 
 export default TopNav;
