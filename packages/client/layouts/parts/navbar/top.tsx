ece6787cf6596232bea844f4ed4e1d96dce30340
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-23 01:44:43 +0800

    use coloured version of logo on expanded navbar
    One side grey the other side coloured is very odd.
    
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/layouts/parts/navbar/top.tsx
+++ packages/client/layouts/parts/navbar/top.tsx
@@ -111,7 +111,7 @@ const TopNav: FC = () => {
             Log out
           </TopNavExpandedItem>
           <div className="flex flex-row justify-between mx-1 mt-6">
-            <Image src="/images/logo-grey.svg" alt="ezkomment" width={40} height={40} />
+            <Image src="/images/logo.svg" alt="ezkomment" width={40} height={40} />
             <ModeSwitcher />
           </div>
         </nav>
