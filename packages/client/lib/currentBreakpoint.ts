8e5314903bbf20828bb6ec628193752c708888c6
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-25 00:02:17 +0800

    dynamically change sort form label based on viewport
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

---
+++ packages/client/lib/currentBreakpoint.ts
@@ -0,0 +1,17 @@
+import theme from "@client/config/tailwind-theme";
+import type { Breakpoint } from "@client/types/utils.type";
+
+/**
+ * Get the current browser breakpoint.
+ *
+ * @returns The current breakpoint of the viewport (consistent with breakpoint prefixes
+ * in Tailwind CSS). If on serverside, returns `unknown`.
+ */
+const currentBreakpoint = (): Breakpoint =>
+    typeof window === "undefined" || !theme.screens
+        ? "unknown"
+        : (Object.entries({ xs: "0px", ...theme.screens }) as [Breakpoint, string][])
+              .reverse()
+              .find(([_, width]) => window.matchMedia(`(min-width: ${width})`).matches)![0];
+
+export default currentBreakpoint;
--- packages/client/types/utils.type.ts
+++ packages/client/types/utils.type.ts
@@ -1,1 +1,2 @@
 export type Mode = "light" | "dark" | "system";
+export type Breakpoint = "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "unknown";
