55a2b29eda0bfa1717166b98359167ac12ae8f91
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-24 21:37:06 +0800

    rename /app to /app/dashboard
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

---
+++ packages/client/pages/app/_middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse, type NextMiddleware } from "next/server";
+
+const middleware: NextMiddleware = req => {
+    const { pathname } = req.nextUrl;
+    if (pathname === "/app") {
+        const url = req.nextUrl.clone();
+        url.pathname = "/app/dashboard";
+        return NextResponse.redirect(url);
+    }
+    return NextResponse.next();
+};
+
+export default middleware;
