acacb06d466fe6b8ebd6b4546d179344b416bcd4
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-24 22:28:45 +0800

    move tailwind theme config to a separate file
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

---
+++ packages/client/config/tailwind-theme.js
@@ -0,0 +1,16 @@
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindTheme } */
+module.exports = {
+    // Screen breakpoints from Bootstrap, https://getbootstrap.com/docs/5.1/layout/breakpoints
+    screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1400px",
+    },
+    container: {
+        center: true,
+        padding: "1.5rem",
+    },
+    extend: {},
+};
--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,21 +1,9 @@
+const theme = require("./config/tailwind-theme");
+
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ["**/*.tsx"],
   darkMode: "class",
-  theme: {
-    // Screen breakpoints from Bootstrap, https://getbootstrap.com/docs/5.1/layout/breakpoints
-    screens: {
-      sm: "576px",
-      md: "768px",
-      lg: "992px",
-      xl: "1200px",
-      "2xl": "1400px",
-    },
-    container: {
-      center: true,
-      padding: "1.5rem",
-    },
-    extend: {},
-  },
+  theme,
   plugins: [require("@tailwindcss/forms")],
 };
