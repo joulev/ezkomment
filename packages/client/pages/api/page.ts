f02a440e8c8091ab431f998db103b47ce8d4b2f0
Vu Van Dung <joulev.vvd@yahoo.com>
2022-03-25 13:07:03 +0800

    remove dummy api routes
    We will import JSON directly, so no need of these guys
    
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/api/page.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiHandler } from "next";
-
-const handler: NextApiHandler = (req, res) => {
-    const json = require("@client/sample/page");
-    res.status(200).json(json);
-};
-
-export default handler;
