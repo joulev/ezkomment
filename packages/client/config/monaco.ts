cbcb8925d71b894d8e1befd6db1ff7eed154bbcd
Vu Van Dung <joulev.vvd@yahoo.com>
2022-04-04 00:08:01 +0800

    use tabSize=2 in monaco editor
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/config/monaco.ts
+++ packages/client/config/monaco.ts
@@ -4,6 +4,7 @@ const monacoOptions: editor.IStandaloneEditorConstructionOptions = {
     minimap: { enabled: false },
     lineHeight: 1.4,
     fontSize: 13,
+    tabSize: 2,
 };
 
 export default monacoOptions;
