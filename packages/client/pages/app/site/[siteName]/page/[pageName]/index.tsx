b93dab00e2c59f8a3606ce24c79a09029770263a
Vu Van Dung <joulev.vvd@yahoo.com>
2022-04-13 22:02:16 +0800

    add modal component to a number of other places
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/app/site/[siteName]/page/[pageName]/index.tsx
+++ packages/client/pages/app/site/[siteName]/page/[pageName]/index.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { formatDistanceToNowStrict, parseISO } from "date-fns";
 import { GetServerSideProps, NextPage } from "next";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 
 import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
 import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
@@ -10,6 +10,8 @@ import WebOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import A from "@client/components/anchor";
 import Banner from "@client/components/banner";
 import Button from "@client/components/buttons";
+import Modal from "@client/components/modal";
+import RightAligned from "@client/components/utils/rightAligned";
 import AppLayout from "@client/layouts/app";
 
 import page from "@client/sample/page.json";
@@ -48,67 +50,94 @@ const Comments: FC<{ comments: Comment[]; children?: ReactNode }> = ({ comments,
   </div>
 );
 
-const PageOverview: NextPage<Props> = ({ page }) => (
-  <AppLayout
-    title={`Page comments | ${site.name}`}
-    type="page"
-    activeTab="all"
-    siteName={site.name}
-    pageId={page.id}
-  >
-    <div className="mx-auto max-w-3xl">
-      <h1 className="mb-3">{page.name}</h1>
-      <div className="flex flex-row gap-3 text-neutral-500">
-        <WebOutlinedIcon />
-        <A
-          href={page.url}
-          notStyled
-          className="hover:text-neutral-900 dark:hover:text-neutral-100 transition whitespace-nowrap text-ellipsis overflow-hidden"
-        >
-          {page.url}
-        </A>
-      </div>
-      <hr />
-      <Banner variant="warning">
-        Beware that if you remove a comment, <strong>that is irreversible</strong> and it will be
-        gone forever.
-      </Banner>
-      <h2>Pending comments</h2>
-      {page.autoApprove && (
-        <p>
-          This page has auto-approval turned on, hence all new comments are automatically approved.
-        </p>
-      )}
-      {page.pendingApprovalComments.length === 0 ? (
-        <p>No pending comments</p>
-      ) : (
-        <>
-          <p>These comments require your approval before appearing in public.</p>
-          <Comments comments={page.pendingApprovalComments}>
-            <div className="flex flex-row gap-3">
-              <Button icon={ClearOutlinedIcon} variant="tertiary" />
-              <Button icon={CheckOutlinedIcon} />
-            </div>
-          </Comments>
-        </>
-      )}
-      <h2>Approved comments</h2>
-      {page.approvedComments.length === 0 ? (
-        <p>No comments</p>
-      ) : (
-        <>
+const PageOverview: NextPage<Props> = ({ page }) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [warningDisabled, setWarningDisabled] = useState(false);
+
+  function handleDelete() {
+    if (!warningDisabled) {
+      setShowDeleteModal(true);
+      setWarningDisabled(true);
+    }
+  }
+
+  return (
+    <AppLayout
+      title={`Page comments | ${site.name}`}
+      type="page"
+      activeTab="all"
+      siteName={site.name}
+      pageId={page.id}
+    >
+      <div className="mx-auto max-w-3xl">
+        <h1 className="mb-3">{page.name}</h1>
+        <div className="flex flex-row gap-3 text-neutral-500">
+          <WebOutlinedIcon />
+          <A
+            href={page.url}
+            notStyled
+            className="hover:text-neutral-900 dark:hover:text-neutral-100 transition whitespace-nowrap text-ellipsis overflow-hidden"
+          >
+            {page.url}
+          </A>
+        </div>
+        <hr />
+        <Banner variant="warning">
+          Beware that if you remove a comment, <strong>that is irreversible</strong> and it will be
+          gone forever.
+        </Banner>
+        <h2>Pending comments</h2>
+        {page.autoApprove && (
           <p>
-            These comments are now live and visible to all visitors of the page. However you can
-            still delete any comments you want.
+            This page has auto-approval turned on, hence all new comments are automatically
+            approved.
           </p>
-          <Comments comments={page.approvedComments}>
-            <Button icon={ClearOutlinedIcon} variant="tertiary" />
-          </Comments>
-        </>
-      )}
-    </div>
-  </AppLayout>
-);
+        )}
+        {page.pendingApprovalComments.length === 0 ? (
+          <p>No pending comments</p>
+        ) : (
+          <>
+            <p>These comments require your approval before appearing in public.</p>
+            <Comments comments={page.pendingApprovalComments}>
+              <div className="flex flex-row gap-3">
+                <Button icon={ClearOutlinedIcon} variant="tertiary" onClick={handleDelete} />
+                <Button icon={CheckOutlinedIcon} />
+              </div>
+            </Comments>
+          </>
+        )}
+        <h2>Approved comments</h2>
+        {page.approvedComments.length === 0 ? (
+          <p>No comments</p>
+        ) : (
+          <>
+            <p>
+              These comments are now live and visible to all visitors of the page. However you can
+              still delete any comments you want.
+            </p>
+            <Comments comments={page.approvedComments}>
+              <Button icon={ClearOutlinedIcon} variant="tertiary" onClick={handleDelete} />
+            </Comments>
+          </>
+        )}
+        <Modal isVisible={showDeleteModal} onOutsideClick={() => setShowDeleteModal(false)}>
+          <div className="p-6 max-w-lg">
+            <p>
+              Since deleted comments are irrecoverable, please be cautious when clicking the buttons
+              to delete the comments here, whether already approved or not.
+            </p>
+            <p>This warning will be silenced for the rest of this session.</p>
+            <RightAligned>
+              <Button icon={CheckOutlinedIcon} onClick={() => setShowDeleteModal(false)}>
+                I understand
+              </Button>
+            </RightAligned>
+          </div>
+        </Modal>
+      </div>
+    </AppLayout>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => ({ props: { page } });
 
--- packages/client/pages/app/site/[siteName]/settings.tsx
+++ packages/client/pages/app/site/[siteName]/settings.tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import DangerousOutlinedIcon from "@mui/icons-material/DangerousOutlined";
 import DnsOutlinedIcon from "@mui/icons-material/DnsOutlined";
 import LabelOutlinedIcon from "@mui/icons-material/LabelOutlined";
@@ -10,6 +12,7 @@ import A from "@client/components/anchor";
 import Button from "@client/components/buttons";
 import CopiableCode from "@client/components/copiableCode";
 import { InputDetachedLabel } from "@client/components/forms/input";
+import Modal from "@client/components/modal";
 import RightAligned from "@client/components/utils/rightAligned";
 import AppLayout from "@client/layouts/app";
 
@@ -18,80 +21,112 @@ import site from "@client/sample/site.json";
 type Site = typeof site;
 type Props = { site: Site };
 
-const SiteSettings: NextPage<Props> = ({ site }) => (
-  <AppLayout
-    title={`Settings | ${site.name}`}
-    type="site"
-    activeTab="settings"
-    siteName={site.name}
-  >
-    <div className="grid md:grid-cols-2 gap-x-12">
-      <div>
-        <h2>Basic information</h2>
-        <form className="flex flex-col gap-6">
-          <InputDetachedLabel
-            label="Site name"
-            icon={LabelOutlinedIcon}
-            type="text"
-            value={site.name}
-            helpText="The site name helps identify your site in the dashboard and the URL."
-            onUpdate={() => {}}
-            required
-          />
-          <InputDetachedLabel
-            label="Site domain"
-            icon={WebOutlinedIcon}
-            type="text"
-            value={site.domain}
-            helpText={
-              <>
-                The domain is where you want to host the comments. It can be any domain or
-                subdomain. Other websites will not be allowed to host the comments. Use an asterisk
-                (<code>*</code>) to allow all domains.
-              </>
-            }
-            onUpdate={() => {}}
-            required
-          />
+const SiteSettings: NextPage<Props> = ({ site }) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  return (
+    <AppLayout
+      title={`Settings | ${site.name}`}
+      type="site"
+      activeTab="settings"
+      siteName={site.name}
+    >
+      <div className="grid md:grid-cols-2 gap-x-12">
+        <div>
+          <h2>Basic information</h2>
+          <form className="flex flex-col gap-6">
+            <InputDetachedLabel
+              label="Site name"
+              icon={LabelOutlinedIcon}
+              type="text"
+              value={site.name}
+              helpText="The site name helps identify your site in the dashboard and the URL."
+              onUpdate={() => {}}
+              required
+            />
+            <InputDetachedLabel
+              label="Site domain"
+              icon={WebOutlinedIcon}
+              type="text"
+              value={site.domain}
+              helpText={
+                <>
+                  The domain is where you want to host the comments. It can be any domain or
+                  subdomain. Other websites will not be allowed to host the comments. Use an
+                  asterisk (<code>*</code>) to allow all domains.
+                </>
+              }
+              onUpdate={() => {}}
+              required
+            />
+            <RightAligned>
+              <Button icon={SaveOutlinedIcon}>Save</Button>
+            </RightAligned>
+          </form>
+          <hr />
+          <h2>Site ID</h2>
+          <p>Your site ID is</p>
+          <CopiableCode content={site.id} className="mb-6" />
+          <p>
+            You can use this to implement your own comment frontend based on the REST API provided
+            by ezkomment. <A href="https://google.com">See more in the documentation</A>.
+          </p>
+          <hr className="md:hidden" />
+        </div>
+        <div>
+          <h2>Export all data</h2>
+          <p>
+            You can request all data related to this site to be exported. You can only perform this
+            action once a day.
+          </p>
           <RightAligned>
-            <Button icon={SaveOutlinedIcon}>Save</Button>
+            <Button icon={DnsOutlinedIcon}>Request data</Button>
           </RightAligned>
-        </form>
-        <hr />
-        <h2>Site ID</h2>
-        <p>Your site ID is</p>
-        <CopiableCode content={site.id} className="mb-6" />
-        <p>
-          You can use this to implement your own comment frontend based on the REST API provided by
-          ezkomment. <A href="https://google.com">See more in the documentation</A>.
-        </p>
-        <hr className="md:hidden" />
+          <hr />
+          <h2>Delete site</h2>
+          <p>
+            This is an <strong>irreversible</strong> action. All site data, including all comments
+            and pages, will be completely erased and there is no way to recover it. All embed and
+            API endpoints for the site will also stop working. Proceed with caution.
+          </p>
+          <RightAligned>
+            <Button
+              variant="danger"
+              icon={DangerousOutlinedIcon}
+              onClick={() => setShowDeleteModal(true)}
+            >
+              Delete this site
+            </Button>
+          </RightAligned>
+          <Modal isVisible={showDeleteModal} onOutsideClick={() => setShowDeleteModal(false)}>
+            <div className="p-6 max-w-lg">
+              <h2>You are attempting a dangerous action.</h2>
+              <p>
+                Deleting a site is <strong>irreversible</strong>, and we cannot do anything to
+                recover any data related to the site, including but not limited to all comments,
+                commenter information, statistics, customised code and access to the REST API for
+                that site.
+              </p>
+              <p>In short, it will be lost permanently. Please think twice before proceeding.</p>
+              <RightAligned className="gap-3">
+                <Button
+                  variant="tertiary"
+                  icon={ArrowBackOutlinedIcon}
+                  onClick={() => setShowDeleteModal(false)}
+                >
+                  Cancel
+                </Button>
+                <Button variant="danger" icon={DangerousOutlinedIcon}>
+                  Delete this site
+                </Button>
+              </RightAligned>
+            </div>
+          </Modal>
+        </div>
       </div>
-      <div>
-        <h2>Export all data</h2>
-        <p>
-          You can request all data related to this site to be exported. You can only perform this
-          action once a day.
-        </p>
-        <RightAligned>
-          <Button icon={DnsOutlinedIcon}>Request data</Button>
-        </RightAligned>
-        <hr />
-        <h2>Delete site</h2>
-        <p>
-          This is an <strong>irreversible</strong> action. All site data, including all comments and
-          pages, will be completely erased and there is no way to recover it. All embed and API
-          endpoints for the site will also stop working. Proceed with caution.
-        </p>
-        <RightAligned>
-          <Button variant="danger" icon={DangerousOutlinedIcon}>
-            Delete this site
-          </Button>
-        </RightAligned>
-      </div>
-    </div>
-  </AppLayout>
-);
+    </AppLayout>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => ({ props: { site } });
 
