3aebdb3e06af6214be2e5c94b18ec8d78a2af257
Vu Van Dung <joulev.vvd@yahoo.com>
2022-04-18 13:37:30 +0800

    add sign in page
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/app/auth/signin.tsx
+++ packages/client/pages/app/auth/signin.tsx
@@ -1,7 +1,10 @@
+import clsx from "clsx";
 import { NextPage } from "next";
 import Image from "next/image";
 
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import GoogleIcon from "@mui/icons-material/Google";
 import KeyOutlinedIcon from "@mui/icons-material/KeyOutlined";
 
 import A from "@client/components/anchor";
@@ -11,23 +14,25 @@ import OrHr from "@client/components/orHr";
 import AuthLayout from "@client/layouts/auth";
 
 const SignUp: NextPage = () => (
-  <AuthLayout title="Sign up with email">
+  <AuthLayout title="Sign in">
     <div className="text-center">
       <A href="/" notStyled>
         <Image src="/images/logo.svg" alt="Logo" width={80} height={80} />
       </A>
-      <h1 className="text-3xl mt-6 mb-12">Create a new account</h1>
+      <h1 className="text-3xl mt-6 mb-12">Sign in to ezkomment</h1>
       <div className="flex flex-col gap-6">
+        <Button icon={GitHubIcon} href="/app/dashboard">
+          Continue with GitHub
+        </Button>
+        <Button icon={GoogleIcon} href="/app/dashboard">
+          Continue with Google
+        </Button>
+        <OrHr className="my-0" />
         <form className="flex flex-col gap-3 w-full">
           <Input icon={EmailOutlinedIcon} placeholder="Email" type="email" required />
           <Input icon={KeyOutlinedIcon} placeholder="Password" type="password" required />
-          <Input icon={KeyOutlinedIcon} placeholder="Confirm password" type="password" required />
-          <Button>Create a new account</Button>
+          <Button className="mt-3">Sign in with email</Button>
         </form>
-        <OrHr className="my-0" />
-        <Button variant="tertiary" href="/app/auth/signup">
-          Other sign up options
-        </Button>
       </div>
     </div>
   </AuthLayout>
