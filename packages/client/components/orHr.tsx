553ec6046faf902d19f8bfbc4c04da054ced7f7f
Vu Van Dung <joulev.vvd@yahoo.com>
2022-04-18 23:12:23 +0800

    create a .bg-card utility (cont.)
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/components/orHr.tsx
+++ packages/client/components/orHr.tsx
@@ -8,7 +8,7 @@ import { FC } from "react";
 const OrHr: FC<{ className?: string }> = ({ className }) => (
   <hr
     className={clsx(
-      "relative after:absolute after:content-['or'] after:bg-white dark:after:bg-black",
+      "relative after:absolute after:content-['or'] after:bg-card",
       "after:px-3 after:-translate-y-1/2 after:-translate-x-1/2 after:text-xs after:uppercase",
       "after:text-muted",
       className
