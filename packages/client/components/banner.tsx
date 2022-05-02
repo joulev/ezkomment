7eff088ec213f081e60ad1e2af5115757ed467d9
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-02 21:01:47 +0700

    add Orbital non-functional warning banner
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/components/banner.tsx
+++ packages/client/components/banner.tsx
@@ -2,13 +2,14 @@ import clsx from "clsx";
 import { FC, ReactNode } from "react";
 
 type BannerVariant = "warning";
+type BannerProps = { variant: BannerVariant; className?: string; children: ReactNode };
 
-const Banner: FC<{ variant: BannerVariant; children: ReactNode }> = ({ variant, children }) => {
+const Banner: FC<BannerProps> = ({ variant, className, children }) => {
   const commonClasses = "p-6 rounded border bg-opacity-20";
   const variantClasses: Record<BannerVariant, string> = {
     warning: "border-amber-500 bg-amber-500",
   };
-  return <div className={clsx(commonClasses, variantClasses[variant])}>{children}</div>;
+  return <div className={clsx(commonClasses, variantClasses[variant], className)}>{children}</div>;
 };
 
 export default Banner;
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,23 +1,41 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 
 import * as home from "@client/components/home/sections";
+import A from "@client/components/anchor";
+import Banner from "@client/components/banner";
+import Button from "@client/components/buttons";
 import Footer from "@client/layouts/parts/footer";
 
-const Home: NextPage = () => (
-  <>
-    <Head>
-      <title>ezkomment: Commenting made easy</title>
-    </Head>
-    <home.Navbar />
-    <home.Banner />
-    <home.PlainHTML />
-    <home.Moderation />
-    <home.Customisation />
-    <home.Api />
-    <home.Ending />
-    <Footer className="px-6 sm:px-10" containerClasses="mx-auto w-full lg:w-5/6 xl:w-4/5" />
-  </>
-);
+const Home: NextPage = () => {
+  const [showWarningBanner, setShowWarningBanner] = useState(true);
+  return (
+    <>
+      <Head>
+        <title>ezkomment: Commenting made easy</title>
+      </Head>
+      <home.Navbar />
+      <home.Banner />
+      <home.PlainHTML />
+      <home.Moderation />
+      <home.Customisation />
+      <home.Api />
+      <home.Ending />
+      <Footer className="px-6 sm:px-10" containerClasses="mx-auto w-full lg:w-5/6 xl:w-4/5" />
+      {showWarningBanner && (
+        <div className="fixed bottom-0 left-0 bg-card border-card rounded m-6">
+          <Banner variant="warning">
+            This site is an <A href="https://orbital.comp.nus.edu.sg">NUS Orbital</A> project. It is
+            not yet functional and is currently under active development.{" "}
+            <Button onClick={() => setShowWarningBanner(false)} className="ml-3">
+              Close
+            </Button>
+          </Banner>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Home;
