573ae08ee5c8a87ae03d1406f258ad5d8c204292
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-04 20:32:33 +0700

    add back <Head> script in _document
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/_document.tsx
+++ packages/client/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 const Document: NextPage = () => (
   <Html lang="en">
-    <Head />
+    <Head>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            const mode = localStorage.getItem("mode");
+            if (mode) {
+              const dark =
+                mode !== "light" &&
+                (mode === "dark" || window.matchMedia("(prefers-color-scheme: dark)").matches);
+              if (dark) document.querySelector("html").classList.add("dark");
+            }`,
+        }}
+      />
+    </Head>
     <body className="bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-200 overflow-y-scroll">
       <Main />
       <NextScript />
