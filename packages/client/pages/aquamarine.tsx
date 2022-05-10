d9d594fbab4ccd6273641b71e7a48b4d2c34dd87
anonymous <anonn@example.com>
2022-05-10 20:14:18 +0700

    .

---
+++ packages/client/pages/aquamarine.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const _: NextPage = () => (
+  <>
+    <Head>
+      <title>アクアマリン</title>
+    </Head>
+    <main
+      className="w-screen h-screen grid place-items-center p-6"
+      style={{ backgroundColor: "aquamarine" }}
+    >
+      <div className="text-black">
+        私は、<b>アクアマリン</b>という色が大好きですから、
+        <br />
+        この特別なページを作ってしまいました（笑）。
+      </div>
+    </main>
+  </>
+);
+
+export default _;
