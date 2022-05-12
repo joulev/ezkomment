7f9d627bf655ba8cca4c296c5b195d387a4c9dc7
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-12 22:10:29 +0700

    ui design update: add transition to orbital page links
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/orbital/index.tsx
+++ packages/client/pages/orbital/index.tsx
@@ -35,7 +35,7 @@ const SectionLink: FC<SectionLinkProps> = ({ icon: Icon, title, href, descriptio
     href={href}
     className={clsx(
       "rounded border border-card bg-card hover:border-neutral-700 dark:hover:border-neutral-300",
-      "p-6 flex flex-col gap-6"
+      "p-6 flex flex-col gap-6 transition"
     )}
   >
     <div className="flex flex-row gap-3 items-center">
