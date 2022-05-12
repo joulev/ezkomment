129676a0899df860f220993efd7159b34f399520
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-12 14:03:33 +0700

    enable truly global custom components for all MDX pages
    That { ...runtime, useMDXComponents } really costed me 1 hour, but after that it feels so satisfying!
    
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { MDXProvider } from "@mdx-js/react";
 import { AppProps } from "next/app";
 
 import BreakpointContext from "@client/context/breakpoint";
@@ -6,6 +7,8 @@ import { useBreakpointInit } from "@client/hooks/breakpoint";
 import useNProgress from "@client/hooks/nprogress";
 import { useModeInit } from "@client/hooks/theme";
 
+import A from "@client/components/anchor";
+import PostHeading from "@client/components/postHeading";
 import { ErrorBoundary } from "@client/layouts/errors";
 
 import "@client/styles/globals.css";
@@ -18,7 +21,19 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ErrorBoundary>
       <ModeContext.Provider value={{ mode, setMode }}>
         <BreakpointContext.Provider value={breakpoint}>
-          <Component {...pageProps} />
+          <MDXProvider
+            components={{
+              a: ({ ref, ...rest }) => <A {...rest} />,
+              h1: props => <PostHeading {...props} level={1} />,
+              h2: props => <PostHeading {...props} level={2} />,
+              h3: props => <PostHeading {...props} level={3} />,
+              h4: props => <PostHeading {...props} level={4} />,
+              h5: props => <PostHeading {...props} level={5} />,
+              h6: props => <PostHeading {...props} level={6} />,
+            }}
+          >
+            <Component {...pageProps} />
+          </MDXProvider>
         </BreakpointContext.Provider>
       </ModeContext.Provider>
     </ErrorBoundary>
--- packages/client/pages/docs/[...slug].tsx
+++ packages/client/pages/docs/[...slug].tsx
@@ -1,20 +1,18 @@
 import * as runtime from "react/jsx-runtime";
 import { compile, nodeTypes, run } from "@mdx-js/mdx";
+import { useMDXComponents } from "@mdx-js/react";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import { ComponentProps, FC, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import rehypeRaw from "rehype-raw";
 import rehypeSlug from "rehype-slug";
 import remarkPrism from "remark-prism";
 
 import { filePaths, getFileData, navData } from "@client/lib/documentation";
 
-import A from "@client/components/anchor";
 import DocsBottomBar from "@client/components/docs/bottombar";
 import DocsSidebar from "@client/components/docs/sidebar";
-import PostHeading from "@client/components/postHeading";
 
-import { PostHeadingProps } from "@client/types/components.type";
 import { DocsData, NavData } from "@client/types/docs.type";
 
 type URLParams = { slug: string[] };
@@ -23,15 +21,12 @@ type PageProps = DocsData & {
   path: string[];
 };
 
-// `components` is just to avoid a warning when that prop is passed when MDX hasn't been rendered
-const Loading: FC<{ components: any }> = ({ components }) => <>Loading</>;
-
 const DocPage: NextPage<PageProps> = ({ title, content, lastModified, path, navData }) => {
   // Sorry for using any here, but since run() in @mdx-js/mdx returns Promise<any>, I can't do anything else.
   const [mdxModule, setMdxModule] = useState<any>(null);
-  const Content = mdxModule ? mdxModule.default : Loading;
+  const Content = mdxModule ? mdxModule.default : () => <>Loading</>;
   useEffect(() => {
-    (async () => setMdxModule(await run(content, runtime)))();
+    (async () => setMdxModule(await run(content, { ...runtime, useMDXComponents })))();
   }, [content]);
   return (
     <>
@@ -43,17 +38,7 @@ const DocPage: NextPage<PageProps> = ({ title, content, lastModified, path, navD
         <main className="col-span-full md:col-span-2 px-6 sm:px-12 py-12 max-w-prose">
           <div style={{ height: "60px" }} className="md:hidden" />
           <article className="post">
-            <Content
-              components={{
-                a: (props: ComponentProps<typeof A>) => <A {...props} />,
-                h1: (props: PostHeadingProps) => <PostHeading {...props} level={1} />,
-                h2: (props: PostHeadingProps) => <PostHeading {...props} level={2} />,
-                h3: (props: PostHeadingProps) => <PostHeading {...props} level={3} />,
-                h4: (props: PostHeadingProps) => <PostHeading {...props} level={4} />,
-                h5: (props: PostHeadingProps) => <PostHeading {...props} level={5} />,
-                h6: (props: PostHeadingProps) => <PostHeading {...props} level={6} />,
-              }}
-            />
+            <Content />
           </article>
           <hr />
           <DocsBottomBar lastModified={lastModified} path={path} />
@@ -81,6 +66,7 @@ const getStaticProps: GetStaticProps<PageProps, URLParams> = async ({ params }) 
             rehypeSlug,
           ],
           remarkPlugins: [remarkPrism],
+          providerImportSource: "@mdx-js/react",
         })
       ),
       path: params?.slug ?? [],
