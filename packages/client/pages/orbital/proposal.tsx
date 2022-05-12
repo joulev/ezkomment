553bf588af7c5b9ee70cd2fa81acb230c5ab8cfb
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-12 13:08:17 +0700

    revert orbital/proposal to using a MDX file directly as a page
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/orbital/proposal.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from "next";
-
-import BlogLayout from "@client/layouts/blog";
-
-import Post, { meta } from "@client/markdown/orbital/proposal.mdx";
-
-const OrbitalProposal: NextPage = () => <BlogLayout {...meta}>{Post}</BlogLayout>;
-
-export default OrbitalProposal;
