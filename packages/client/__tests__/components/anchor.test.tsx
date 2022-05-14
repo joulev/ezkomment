2e66719bf390582be21dd3be241c720fd1dd824c
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-14 08:57:43 +0700

    set up testing with jest and react-testing-library
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

---
+++ packages/client/__tests__/components/anchor.test.tsx
@@ -0,0 +1,54 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import A from "@client/components/anchor";
+
+describe("Anchor component", () => {
+  it("`href` tests", () => {
+    render(
+      <>
+        <A>fake link</A>
+        <A href="#">anchor</A>
+        <A href="/">internal</A>
+        <A href="https://google.com">external</A>
+      </>
+    );
+
+    expect(screen.getByText("fake link")).toBeInTheDocument();
+    expect(screen.getByText("fake link")).not.toHaveAttribute("href");
+
+    expect(screen.getByText("anchor")).toBeInTheDocument();
+    expect(screen.getByText("anchor")).toHaveAttribute("href", "#");
+    expect(screen.getByText("anchor")).not.toHaveAttribute("target");
+
+    expect(screen.getByText("internal")).toBeInTheDocument();
+    expect(screen.getByText("internal")).toHaveAttribute("href", "/");
+    expect(screen.getByText("internal")).not.toHaveAttribute("target");
+
+    expect(screen.getByText("external")).toBeInTheDocument();
+    expect(screen.getByText("external")).toHaveAttribute("href", "https://google.com");
+    expect(screen.getByText("external")).toHaveAttribute("target", "_blank");
+  });
+
+  it("`notStyled` tests", () => {
+    render(
+      <>
+        <A>Normal link</A>
+        <A notStyled>Not styled</A>
+      </>
+    );
+
+    expect(screen.getByText("Normal link")).toBeInTheDocument();
+    expect(screen.getByText("Normal link")).toHaveClass("a");
+
+    expect(screen.getByText("Not styled")).toBeInTheDocument();
+    expect(screen.getByText("Not styled")).not.toHaveClass("a");
+  });
+
+  it("`className` tests", () => {
+    render(<A className="custom-class">Custom class</A>);
+    expect(screen.getByText("Custom class")).toBeInTheDocument();
+    expect(screen.getByText("Custom class")).toHaveClass("a");
+    expect(screen.getByText("Custom class")).toHaveClass("custom-class");
+  });
+});
---
+++ packages/client/jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig = {
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  moduleNameMapper: {
+    "^@client/components/(.*)$": "<rootDir>/components/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
