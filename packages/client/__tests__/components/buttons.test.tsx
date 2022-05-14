32b9e8ab3e0ecf70ec468c2b4359c8202a138f2f
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-14 11:14:36 +0700

    add more test cases for buttons component
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/__tests__/components/buttons.test.tsx
+++ packages/client/__tests__/components/buttons.test.tsx
@@ -36,4 +36,51 @@ describe("Button component", () => {
       "text-neutral-700 dark:text-neutral-300 border-card bg-card"
     );
   });
+
+  it("`href` testing", () => {
+    expect(() =>
+      render(
+        <>
+          <Button id="no-href">No href</Button>
+          <Button id="href" href="https://google.com">
+            Href
+          </Button>
+        </>
+      )
+    ).not.toThrow();
+
+    expect(screen.getByText("No href")).toBeInTheDocument();
+    expect(document.getElementById("no-href")).not.toHaveAttribute("href");
+
+    expect(screen.getByText("Href")).toBeInTheDocument();
+    expect(document.getElementById("href")).toHaveAttribute("href", "https://google.com");
+    expect(document.getElementById("href")).toHaveAttribute("target", "_blank");
+  });
+
+  it("`className` testing", () => {
+    expect(() =>
+      render(
+        <Button id="class-name" className="test">
+          Class name
+        </Button>
+      )
+    ).not.toThrow();
+    expect(document.getElementById("class-name")).toHaveClass("test");
+  });
+
+  it("`children` testing", () => {
+    expect(() =>
+      render(
+        <>
+          <Button id="no-children" />
+          <Button id="have-children">Text</Button>
+        </>
+      )
+    ).not.toThrow();
+
+    expect(document.getElementById("no-children")).toHaveClass("p-1.5");
+
+    expect(screen.getByText("Text")).toBeInTheDocument();
+    expect(document.getElementById("have-children")).not.toHaveClass("p-1.5");
+  });
 });
