36906cfb03dad63e3b5c1bdf0018139798958c4b
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-19 17:36:02 +0700

    merge AuthLayout to the auth page
    We don't need a separate layout for one single page
    
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/auth.tsx
+++ packages/client/pages/auth.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+import Head from "next/head";
 import Image from "next/image";
 import { MouseEvent, ReactNode, useState } from "react";
 
@@ -9,9 +11,10 @@ import { githubProvider, googleProvider, signIn } from "@client/lib/firebase/aut
 
 import A from "@client/components/anchor";
 import AuthError from "@client/components/auth/error";
+import AuthProvider from "@client/components/auth/provider";
 import Banner from "@client/components/banner";
 import Button from "@client/components/buttons";
-import AuthLayout from "@client/layouts/auth";
+import LoadingBanner from "@client/components/loadingBanner";
 
 import { Provider } from "@client/types/auth.type";
 import { NextPageWithLayout } from "@client/types/utils.type";
@@ -33,24 +36,39 @@ const Auth: NextPageWithLayout = () => {
   };
 
   return (
-    <div className="text-center">
-      <A href="/" notStyled>
-        <Image src={logo} alt="Logo" width={80} height={80} />
-      </A>
-      <h1 className="text-3xl mt-6 mb-12">Continue to ezkomment</h1>
-      <div className="flex flex-col gap-6">
-        {error && <Banner variant="error">{error}</Banner>}
-        <Button icon={GitHubIcon} onClick={handler(githubProvider)}>
-          Continue with GitHub
-        </Button>
-        <Button icon={GoogleIcon} onClick={handler(googleProvider)}>
-          Continue with Google
-        </Button>
-      </div>
-    </div>
+    <>
+      <Head>
+        <title>Authentication | ezkomment</title>
+      </Head>
+      <main className="h-screen grid place-items-center">
+        <div
+          className={clsx(
+            "w-full h-full px-6 py-12 sm:py-6 sm:w-96 sm:h-auto mx-auto",
+            "bg-card sm:border sm:border-neutral-300 dark:sm:border-neutral-700 sm:rounded"
+          )}
+        >
+          <div className="text-center">
+            <A href="/" notStyled>
+              <Image src={logo} alt="Logo" width={80} height={80} />
+            </A>
+            <h1 className="text-3xl mt-6 mb-12">Continue to ezkomment</h1>
+            <div className="flex flex-col gap-6">
+              {error && <Banner variant="error">{error}</Banner>}
+              <Button icon={GitHubIcon} onClick={handler(githubProvider)}>
+                Continue with GitHub
+              </Button>
+              <Button icon={GoogleIcon} onClick={handler(googleProvider)}>
+                Continue with Google
+              </Button>
+            </div>
+          </div>
+        </div>
+      </main>
+      <LoadingBanner />
+    </>
   );
 };
 
-Auth.getLayout = page => <AuthLayout title="Authentication">{page}</AuthLayout>;
+Auth.getLayout = page => <AuthProvider>{page}</AuthProvider>;
 
 export default Auth;
