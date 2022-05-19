ae83c74535e2e61151a05f871021e9928125f752
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-19 16:41:48 +0700

    nest layouts using auth under the same context provider
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/layouts/app.tsx
+++ packages/client/layouts/app.tsx
@@ -2,28 +2,23 @@ import clsx from "clsx";
 import Head from "next/head";
 import { FC } from "react";
 
-import AuthContext from "@client/context/auth";
-import { useAuthInit } from "@client/hooks/auth";
-
 import Navbar from "@client/components/app/navbar";
+import AuthProvider from "@client/components/authProvider";
 import Footer from "@client/components/footer";
 import LoadingBanner from "@client/components/loadingBanner";
 
 import { AppProps } from "@client/types/components.type";
 
-const AppLayout: FC<AppProps> = ({ title, removePadding, children, ...rest }) => {
-  const providedAuth = useAuthInit();
-  return (
-    <AuthContext.Provider value={providedAuth}>
-      <Head>
-        <title>{title} | ezkomment</title>
-      </Head>
-      <Navbar {...rest} />
-      <main className={clsx("container", removePadding || "py-9")}>{children}</main>
-      <Footer />
-      <LoadingBanner />
-    </AuthContext.Provider>
-  );
-};
+const AppLayout: FC<AppProps> = ({ title, removePadding, children, ...rest }) => (
+  <AuthProvider>
+    <Head>
+      <title>{title} | ezkomment</title>
+    </Head>
+    <Navbar {...rest} />
+    <main className={clsx("container", removePadding || "py-9")}>{children}</main>
+    <Footer />
+    <LoadingBanner />
+  </AuthProvider>
+);
 
 export default AppLayout;
--- packages/client/layouts/auth.tsx
+++ packages/client/layouts/auth.tsx
@@ -2,31 +2,26 @@ import clsx from "clsx";
 import Head from "next/head";
 import { FC, ReactNode } from "react";
 
-import AuthContext from "@client/context/auth";
-import { useAuthInit } from "@client/hooks/auth";
-
+import AuthProvider from "@client/components/authProvider";
 import LoadingBanner from "@client/components/loadingBanner";
 
-const AuthLayout: FC<{ title: string; children: ReactNode }> = ({ title, children }) => {
-  const providedAuth = useAuthInit();
-  return (
-    <AuthContext.Provider value={providedAuth}>
-      <Head>
-        <title>{title} | ezkomment</title>
-      </Head>
-      <main className="h-screen grid place-items-center">
-        <div
-          className={clsx(
-            "w-full h-full px-6 py-12 sm:py-6 sm:w-96 sm:h-auto mx-auto",
-            "bg-card sm:border sm:border-neutral-300 dark:sm:border-neutral-700 sm:rounded"
-          )}
-        >
-          {children}
-        </div>
-      </main>
-      <LoadingBanner />
-    </AuthContext.Provider>
-  );
-};
+const AuthLayout: FC<{ title: string; children: ReactNode }> = ({ title, children }) => (
+  <AuthProvider>
+    <Head>
+      <title>{title} | ezkomment</title>
+    </Head>
+    <main className="h-screen grid place-items-center">
+      <div
+        className={clsx(
+          "w-full h-full px-6 py-12 sm:py-6 sm:w-96 sm:h-auto mx-auto",
+          "bg-card sm:border sm:border-neutral-300 dark:sm:border-neutral-700 sm:rounded"
+        )}
+      >
+        {children}
+      </div>
+    </main>
+    <LoadingBanner />
+  </AuthProvider>
+);
 
 export default AuthLayout;
