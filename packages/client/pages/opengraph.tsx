ede6335406131b944ac1cf87dd6ba1f7e5a373f4
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-24 14:02:49 +0700

    add a "default" fallback for opengraph image
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/opengraph.tsx
+++ packages/client/pages/opengraph.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useRouter } from "next/router";
 
 import { NextPageWithLayout } from "@client/types/utils.type";
@@ -5,7 +6,25 @@ import { NextPageWithLayout } from "@client/types/utils.type";
 const OpenGraphImageGenerator: NextPageWithLayout = () => {
   const router = useRouter();
   const title = router.query.title as string | undefined;
-  if (!title) return null;
+  if (!title)
+    return (
+      <div
+        style={{ width: 1200, height: 630, gap: 48, fontSize: 80 }}
+        className="flex flex-col justify-center items-center bg-gradient-to-br from-indigo-400 to-indigo-600"
+      >
+        <div>
+          <img
+            src="/images/logo-text-white.svg"
+            alt="you shouldn't see this"
+            width={397}
+            height={80}
+          />
+        </div>
+        <div className="font-extralight text-white">
+          Commenting made <span className="font-extrabold">easy</span>.
+        </div>
+      </div>
+    );
   return (
     <div
       style={{
@@ -15,10 +34,9 @@ const OpenGraphImageGenerator: NextPageWithLayout = () => {
         fontSize: title.length > 32 ? 72 : 80,
         gap: 48,
       }}
-      className="bg-gradient-to-br from-indigo-400 to-indigo-600 text-neutral-100 flex flex-col justify-center"
+      className="bg-gradient-to-br from-indigo-400 to-indigo-600 text-white flex flex-col justify-center"
     >
       <div className="flex flex-row divide-x divide-neutral-100 items-center" style={{ gap: 32 }}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="/images/logo-text-white.svg"
           alt="you shouldn't see this"
