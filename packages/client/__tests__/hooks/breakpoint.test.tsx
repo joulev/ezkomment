6cfdf07f7f5461136814c337eff34f4e382fb45c
Vu Van Dung <joulev.vvd@yahoo.com>
2022-05-26 23:17:50 +0700

    add tests for navbar; remove the last missed line from testing
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/__tests__/hooks/breakpoint.test.tsx
+++ packages/client/__tests__/hooks/breakpoint.test.tsx
@@ -5,39 +5,7 @@ import { FC, ReactNode } from "react";
 import theme from "@client/config/tailwindTheme";
 import BreakpointContext from "@client/context/breakpoint";
 import useBreakpoint, { useBreakpointInit } from "@client/hooks/breakpoint";
-
-function queryMatchesCurrentWidth(query: string, currentWidth: number) {
-  const regexp = /^\(min-width:\s(\d+)px\)$/g;
-  const matches = query.match(regexp);
-  if (!matches) return false;
-  const queryValue = parseInt(matches[0].replace(regexp, "$1"));
-  return currentWidth >= queryValue;
-}
-
-/**
- * @see https://stackoverflow.com/a/53449595
- */
-function setupMedia(currentWidth: number) {
-  Object.defineProperty(window, "matchMedia", {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-      matches: queryMatchesCurrentWidth(query, currentWidth),
-      media: query,
-      onchange: null,
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
-  });
-}
-
-describe("Test this test's setup functions `setupMedia` and `queryMatchesCurrentWidth`", () => {
-  it("Never thought I'd see a test for another test", () => {
-    setupMedia(800);
-    expect(window.matchMedia("(min-width: 800px)").matches).toBeTruthy();
-    expect(window.matchMedia("(min-width: 801px)").matches).toBeFalsy();
-  });
-});
+import setupMediaViewport from "@client/lib/tests/setupMediaViewport";
 
 const Document: FC<{ children: ReactNode }> = ({ children }) => {
   const breakpoint = useBreakpointInit();
@@ -63,13 +31,13 @@ const breakpoints = Object.entries(screens).map(([breakpoint, width]) => ({
 
 describe("`breakpoint` integration test", () => {
   it("Test value on load based on viewport width", () => {
-    setupMedia(500); // xs
+    setupMediaViewport(500); // xs
     render(<Page />);
     expect(screen.getByTestId("breakpoint")).toHaveTextContent("xs");
     document.body.innerHTML = "";
 
     breakpoints.forEach(({ breakpoint, width }) => {
-      setupMedia(width);
+      setupMediaViewport(width);
       render(<Page />);
       expect(screen.getByTestId("breakpoint")).toHaveTextContent(breakpoint);
       document.body.innerHTML = "";
