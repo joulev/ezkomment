07fe0ff4f5126efe6a023e9a6e0c70331b90416a
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-02 13:57:33 +0700

    revert logging since we don't need it any more
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/lib/getOgImage.ts
+++ packages/client/lib/getOgImage.ts
@@ -12,29 +12,21 @@ export default async function getOgImage({ title, label }: OgImageProps) {
     const filePath = `${dir}/${hash}.png`;
     const publicPath = `https://${process.env.VERCEL_URL}/images/og/${hash}.png`;
 
-    if (existsSync(filePath)) {
-        console.log(`Getting image for "${title}" from cache successfully`);
-        return publicPath;
-    }
+    if (existsSync(filePath)) return publicPath;
 
     const url = new URL("https://ezkomment-67rtb9jff-joulev.vercel.app/opengraph");
     if (title) url.searchParams.append("title", title);
     if (label) url.searchParams.append("label", label);
 
-    try {
-        const buffer = await captureWebsite.buffer(url.href, {
-            width: 1200,
-            height: 630,
-            scaleFactor: 1,
-            // https://github.com/sindresorhus/capture-website#im-getting-a-sandbox-related-error
-            launchOptions: { args: ["--no-sandbox", "--disable-setuid-sandbox"] },
-        });
+    const buffer = await captureWebsite.buffer(url.href, {
+        width: 1200,
+        height: 630,
+        scaleFactor: 1,
+        // https://github.com/sindresorhus/capture-website#im-getting-a-sandbox-related-error
+        launchOptions: { args: ["--no-sandbox", "--disable-setuid-sandbox"] },
+    });
 
-        mkdirSync(dir, { recursive: true });
-        writeFileSync(filePath, buffer);
-        console.log(`Capturing image for "${title}" successfully`);
-    } catch (error) {
-        console.error(`Error while capturing image for "${title}":`, error);
-    }
+    mkdirSync(dir, { recursive: true });
+    writeFileSync(filePath, buffer);
     return publicPath;
 }
