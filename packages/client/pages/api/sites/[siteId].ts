51c67d3fae930145d2af6def072976ad0bead50c
VietAnh1010 <vietanh101003@gmail.com>
2022-06-02 21:19:42 +0700

    Failed to integrate API to `@client`, revert back ...

--- packages/client/pages/api/sites/[siteId].ts
+++
@@ -1,8 +0,0 @@
-import * as SiteHandler from "@server/handlers/siteHandler";
-import { createNextHandler } from "@server/utils/extraUtils";
-
-export default createNextHandler({
-    GET: SiteHandler.getSite,
-    POST: SiteHandler.updateSite,
-    DELETE: SiteHandler.deleteSite,
-});
--- packages/client/pages/api/users/[uid]/sites.ts
+++
@@ -1,6 +0,0 @@
-import * as UserHandlers from "@server/handlers/userHandlers";
-import { createNextHandler } from "@server/utils/extraUtils";
-
-export default createNextHandler({
-    GET: UserHandlers.listUserSites,
-});
