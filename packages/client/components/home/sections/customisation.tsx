09d27926b44a2accbcbb3045049e18b38597abb5
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-03 16:47:06 +0700

    hot fix, close #78
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/components/home/sections/customisation.tsx
+++ packages/client/components/home/sections/customisation.tsx
@@ -1,7 +1,4 @@
-import { FC, useEffect, useRef, useState } from "react";
-
-import useTheme from "@client/hooks/theme";
-import generateCommentHTML from "@client/lib/generateCommentHTML";
+import { FC } from "react";
 
 import A from "@client/components/anchor";
 import Button from "@client/components/buttons";
@@ -9,66 +6,11 @@ import CodeBlock from "@client/components/home/codeblock";
 import Section from "@client/components/home/section";
 import Window from "@client/components/home/window";
 
-import { all, comment, styles } from "@client/constants/sampleCommentCode";
-
-function useIframe() {
-  const [contentHeight, setContentHeight] = useState(0);
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-
-  const handler = async () => {
-    if (iframeRef.current) {
-      // Wait for the iframe to render? Without it, the height is 0 for about (shockingly) 70% of
-      // the time. Yeap, neither 0% nor 100%. Never think what I do would depend on pure luck but
-      // here we go.
-      await new Promise(resolve => setTimeout(resolve, 50));
-      setContentHeight(iframeRef.current.contentWindow?.document.body.clientHeight ?? 0);
-    }
-  };
-
-  useEffect(() => {
-    handler();
-  }, [iframeRef]);
-
-  useEffect(() => {
-    window.addEventListener("resize", handler);
-    return () => window.removeEventListener("resize", handler);
-  }, []);
-
-  return { contentHeight, iframeRef };
-}
-
 const Illustration: FC<{ codeHtml: string }> = ({ codeHtml }) => {
-  const theme = useTheme();
-  const { contentHeight, iframeRef } = useIframe();
   return (
-    <div className="relative h-[480px]">
-      <div className="absolute top-0 inset-x-0 scale-75 origin-top-left">
-        <Window tabs={["index.html", "comment.html", "styles.css"]} activeTab={0}>
-          <CodeBlock codeHtml={codeHtml} />
-        </Window>
-      </div>
-      <div className="absolute bottom-0 inset-x-0 p-6 rounded border border-card bg-card scale-75 origin-bottom-right pointer-events-none">
-        <iframe
-          srcDoc={generateCommentHTML(all, comment, styles, theme === "dark")}
-          className="w-full"
-          style={{ height: contentHeight }}
-          ref={iframeRef}
-        />
-      </div>
-      <svg width={61} height={106} className="absolute bottom-24 left-[3%] sm:left-[6%]">
-        {["M 1 1 C 1 50 20 90 60 100", "M 40 105 L 60 100 L 47 84"].map((path, i) => (
-          <path
-            d={path}
-            className="stroke-indigo-500"
-            fill="none"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            key={i}
-          />
-        ))}
-      </svg>
-    </div>
+    <Window tabs={["index.html", "comment.html", "styles.css"]} activeTab={0}>
+      <CodeBlock codeHtml={codeHtml} />
+    </Window>
   );
 };
 
