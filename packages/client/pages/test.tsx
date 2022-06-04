6c0fd7c85d06536a2e2ecddf3fcaffaf493a554d
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-04 10:54:01 +0700

    apparently working, remove the test page
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- packages/client/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-// This page will crash
-
-export default function Test() {
-  const [obj, setObj] = useState<any>("test");
-  useEffect(() => setObj({ a: 1, b: 2 }), []);
-  return <div>{obj}</div>;
-}
