4dee32bc22cc67c402dc9761175c32668ed9d8af
VietAnh1010 <vietanh101003@gmail.com>
2022-06-09 22:06:11 +0700

    Deleted `packages/server`. I will use symlink for easier navigation on my local machine.

--- packages/server/pages/api/pages/[pageId]/comments.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import nc from "next-connect";
-
-import { createPageComment } from "@server/handlers/pageHandlers";
-
-const hander = nc<NextApiRequest, NextApiResponse>().post(createPageComment);
-
-export default hander;
