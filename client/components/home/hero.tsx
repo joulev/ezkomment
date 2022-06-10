4d6066e8001da2c0e4b56bf22e497af5f8291244
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-10 21:43:00 +0700

    improve the hero section design
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- client/components/home/hero.tsx
+++ client/components/home/hero.tsx
@@ -3,49 +3,50 @@ import { FC } from "react";
 
 import useTheme from "~/client/hooks/theme";
 
+import Button from "~/client/components/buttons";
+
 import darkScreen from "~/public/images/home/app-dark.png";
 import lightScreen from "~/public/images/home/app-light.png";
 import logoText from "~/public/images/logo-text.svg";
 
-import Button from "../buttons";
-
 const Hero: FC = () => {
   const mode = useTheme();
   return (
-    <div className="overflow-x-hidden mt-24">
-      <div className="container grid grid-cols-1 md:grid-cols-2 gap-12">
-        <div className="text-center md:text-left flex flex-col gap-12">
+    <section className="overflow-x-hidden my-12 lg:my-18">
+      <div className="container grid grid-cols-1 md:grid-cols-12 gap-6 xl:gap-12 items-center">
+        <div className="md:col-span-7 text-center md:text-left flex flex-col gap-9 lg:gap-12">
           <div>
-            <div className="logo-width inline-block">
-              <Image src={logoText} alt="logo" layout="responsive" width={397} height={80} />
+            <div className="w-[calc(397px/80*36)] h-9 lg:w-[calc(397px/80*48)] lg:h-12 inline-block">
+              <Image src={logoText} alt="logo" layout="responsive" />
             </div>
           </div>
-          <h1 className="font-black text-5xl md:text-6xl xl:text-7xl !leading-[0.85] my-0">
+          <h1 className="font-black text-5xl lg:text-6xl xl:text-7xl !leading-[0.85] my-0">
             Commenting made&nbsp;
             <span className="bg-gradient-to-r from-blue-500 to-violet-500 bg-clip-text text-transparent">
               easy
             </span>
           </h1>
-          <div className="text-muted text-xl lg:text-2xl">
+          <div className="text-neutral-600 dark:text-neutral-400 text-lg md:text-xl lg:text-2xl">
             No complicated backend configuration. Add a comment section anywhere &ndash; even if you
             use plain HTML, we got you covered.
           </div>
           <div>
-            <Button href="/auth" className="inline-block text-lg lg:text-xl px-9 py-3">
+            <Button href="/auth" className="inline-block lg:text-xl px-9 py-3">
               Get started
             </Button>
           </div>
         </div>
-        <div className="hidden md:block relative h-[calc(1612px/2.5)]">
-          <div className="absolute left-0 inset-y-0 w-[calc(2212px/2.5)]">
+        <div className="hidden md:block md:col-span-5 relative h-[calc(1590px/3.9)] lg:h-[calc(1590px/2.5)]">
+          <div className="absolute left-0 inset-y-0 w-[calc(2120px/4)] lg:w-[calc(2120px/2.6)]">
             <Image
               src={mode === "dark" ? darkScreen : lightScreen}
               alt="Site dashboard screenshot"
+              priority
             />
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
