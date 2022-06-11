59c657d89137ac169b34ca872de28112f3fe618b
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-11 17:00:38 +0700

    add initial draft for illustration animation with framer-motion
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- client/components/home/easy/configuration.tsx
+++ client/components/home/easy/configuration.tsx
@@ -1,12 +1,54 @@
-import { FC } from "react";
+import { Variants, motion, useAnimation } from "framer-motion";
+import Image from "next/image";
+import { FC, ReactNode, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
-import A from "~/client/components/anchor";
+import useTheme from "~/client/hooks/theme";
+
+import createNewPage from "~/public/images/home/create-new-page.svg";
+import createNewSite from "~/public/images/home/create-new-site.svg";
+import sectionLiveDark from "~/public/images/home/section-live-dark.svg";
+import sectionLiveLight from "~/public/images/home/section-live-light.svg";
 
 import ButtonLink from "../buttonLink";
 
-const EasyConfiguration: FC = () => {
+const Illustration: FC<{ children: ReactNode }> = ({ children }) => {
+  const animation = useAnimation();
+  const [ref, inView] = useInView();
+  const variants: Variants = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: 50,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+      },
+    },
+  };
+  useEffect(() => {
+    if (inView) {
+      animation.start("visible");
+    }
+  }, [animation, inView]);
   return (
-    <section className="container my-24 grid md:grid-cols-2 gap-6">
+    <motion.div ref={ref} variants={variants} initial="hidden" animate={animation}>
+      {children}
+    </motion.div>
+  );
+};
+
+const EasyConfiguration: FC = () => {
+  const theme = useTheme();
+  return (
+    <section className="container my-24 grid md:grid-cols-2 gap-12">
       <div className="flex flex-col gap-9">
         <h2 className="my-0 font-black text-4xl lg:text-5xl">
           <span className="text-gradient from-red-500 to-orange-500">easy</span> configuration
@@ -23,6 +65,35 @@ const EasyConfiguration: FC = () => {
             Create your first
           </ButtonLink>
         </div>
+      </div>
+      <div className="flex flex-col gap-12">
+        <Illustration>
+          <div className="mx-auto" style={{ width: 200 }}>
+            <Image
+              src={createNewSite}
+              alt="Cursor clicking on a 'Create a new site' button"
+              layout="responsive"
+            />
+          </div>
+        </Illustration>
+        <Illustration>
+          <div className="mx-auto" style={{ width: 200 }}>
+            <Image
+              src={createNewPage}
+              alt="Cursor clicking on a 'Create a new page' button"
+              layout="responsive"
+            />
+          </div>
+        </Illustration>
+        <Illustration>
+          <div>
+            <Image
+              src={theme === "dark" ? sectionLiveDark : sectionLiveLight}
+              alt="Banner showing creating comment section is successful"
+              layout="responsive"
+            />
+          </div>
+        </Illustration>
       </div>
     </section>
   );
