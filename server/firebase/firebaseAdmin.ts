3df8f92f297631d513968b75af3d2404c17d93d0
VietAnh1010 <vietanh101003@gmail.com>
2022-06-12 14:46:31 +0700

    Added Firebase Storage

--- server/firebase/firebaseAdmin.ts
+++ server/firebase/firebaseAdmin.ts
@@ -2,6 +2,7 @@ import * as admin from "firebase-admin";
 import { cert, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
 
 const serviceAccount = {
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -13,9 +14,11 @@ const firebaseAdmin = admin.apps.length
     ? admin.app()
     : initializeApp({
           credential: cert(serviceAccount),
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       });
 
 const firestoreAdmin = getFirestore(firebaseAdmin);
 const authAdmin = getAuth(firebaseAdmin);
+const bucketAdmin = getStorage(firebaseAdmin).bucket();
 
-export { firestoreAdmin, authAdmin };
+export { firestoreAdmin, authAdmin, bucketAdmin };
---
+++ server/utils/photoUtils.ts
@@ -0,0 +1,4 @@
+import { bucketAdmin } from "~/server/firebase/firebaseAdmin";
+
+export async function uploadUserPhoto(file: any) {}
+export async function uploadSiteIcon(file: any) {}
