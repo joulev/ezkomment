858a58192260fc4b9d0d396509d4d6d1998f7bb3
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-12 15:32:18 +0700

    add landing page sections more room to breathe
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- client/components/home/section.tsx
+++ client/components/home/section.tsx
@@ -7,7 +7,7 @@ import ButtonLink from "./buttonLink";
 import Illustration from "./illustration";
 
 const HomeSection: FC<HomeSectionProps> = ({ colourClass, title, desc, button, illustration }) => (
-  <section className="container my-24 grid md:grid-cols-2 gap-18">
+  <section className="container my-24 md:mt-36 md:mb-48 grid md:grid-cols-2 gap-18">
     <div className="flex flex-col gap-9">
       <h2 className="my-0 font-black text-4xl lg:text-5xl">
         <span className={clsx("text-gradient", colourClass)}>easy</span>
