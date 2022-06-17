c477c5ba7cf76564efb5c7766a0301019298d4b3
VietAnh1010 <vietanh101003@gmail.com>
2022-06-17 22:50:01 +0700

    Sanitize user display name

--- server/middlewares/sanitizeRequests/users.ts
+++ server/middlewares/sanitizeRequests/users.ts
@@ -1,9 +1,15 @@
+import validator from "validator";
+
+import CustomApiError from "~/server/utils/errors/customApiError";
+
 import { ApiMiddleware } from "~/types/server/nextApi.type";
 import { UpdateUserBodyParams } from "~/types/server/user.type";
 
 export const sanitizeUpdateUserRequest: ApiMiddleware = (req, _, next) => {
     const { displayName }: UpdateUserBodyParams = req.body;
-    // Should I write rules to validate user's display name? Probably yes.
+    if (typeof displayName !== "string" || validator.isEmpty(displayName)) {
+        throw new CustomApiError("'displayName' must be a non-empty string");
+    }
     req.body = { displayName };
     next();
 };
