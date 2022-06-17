eae475b2931c0403ff68f80edfebca828d808ae4
VietAnh1010 <vietanh101003@gmail.com>
2022-06-17 21:40:59 +0700

    Ensure uniqueness of names

--- server/utils/crud/siteUtils.ts
+++ server/utils/crud/siteUtils.ts
@@ -1,7 +1,10 @@
+/**
+ * We also need to ensure the uniqueness of site's name.
+ */
 import { firestoreAdmin } from "~/server/firebase/firebaseAdmin";
 import CustomApiError from "~/server/utils/errors/customApiError";
 
-import { CreateSiteRequest, UpdateSiteBodyParams } from "~/types/server";
+import { CreateSiteRequest, Site, UpdateSiteBodyParams } from "~/types/server";
 
 import { handleFirestoreError } from "../errors/handleFirestoreError";
 import { deleteRefArray } from "../firestoreUtils";
@@ -11,6 +14,7 @@ import { deleteSitePagesById } from "./pageUtils";
  * The collection of sites.
  */
 const SITES_COLLECTION = firestoreAdmin.collection("sites");
+const USERS_COLLECTION = firestoreAdmin.collection("users");
 
 /**
  * Get a site with the given id.
@@ -38,14 +42,21 @@ export async function getSiteById(siteId: string) {
  */
 export async function createSite(data: CreateSiteRequest) {
     try {
+        const { uid, name } = data;
         const siteRef = SITES_COLLECTION.doc();
         const siteId = siteRef.id;
         const newSite = {
             id: siteId,
             ...data,
         };
-        await siteRef.create(newSite);
-        return newSite;
+        return await firestoreAdmin.runTransaction(async t => {
+            /**
+             * Ensure uniqueness.
+             */
+            t.create(USERS_COLLECTION.doc(uid).collection("sites").doc(name), { id: siteId });
+            t.create(siteRef, newSite);
+            return newSite;
+        });
     } catch (err) {
         handleFirestoreError(err);
     }
@@ -57,7 +68,26 @@ export async function createSite(data: CreateSiteRequest) {
  */
 export async function updateSiteById(siteId: string, data: UpdateSiteBodyParams) {
     try {
-        return await SITES_COLLECTION.doc(siteId).update(data);
+        const siteRef = SITES_COLLECTION.doc(siteId);
+        const newName = data.name;
+        return await firestoreAdmin.runTransaction(async t => {
+            if (newName !== undefined) {
+                // Look up the site's name
+                const siteSnapshot = await t.get(siteRef);
+                if (!siteSnapshot.exists) {
+                    throw new CustomApiError("Site does not exist", 404);
+                }
+                const siteData = siteSnapshot.data() as Site;
+                const oldName = siteData.name;
+                const uid = siteData.uid;
+                // Update the name list
+                t.delete(USERS_COLLECTION.doc(uid).collection("sites").doc(oldName));
+                t.create(USERS_COLLECTION.doc(uid).collection("sites").doc(newName), {
+                    id: siteId,
+                });
+            }
+            t.update(siteRef, data);
+        });
     } catch (err) {
         handleFirestoreError(err);
     }
@@ -69,7 +99,16 @@ export async function updateSiteById(siteId: string, data: UpdateSiteBodyParams)
  */
 export async function deleteSiteById(siteId: string) {
     try {
-        return await SITES_COLLECTION.doc(siteId).delete({ exists: true });
+        const siteRef = SITES_COLLECTION.doc(siteId);
+        return await firestoreAdmin.runTransaction(async t => {
+            const siteSnapshot = await t.get(siteRef);
+            if (!siteSnapshot.exists) {
+                throw new CustomApiError("Site does not exist", 404);
+            }
+            const { name, uid } = siteSnapshot.data() as Site;
+            t.delete(USERS_COLLECTION.doc(uid).collection("sites").doc(name));
+            t.delete(siteRef);
+        });
     } catch (err) {
         handleFirestoreError(err);
     }
@@ -91,10 +130,17 @@ export async function listUserSitesById(uid: string) {
 export async function deleteUserSitesById(uid: string) {
     try {
         const siteSnapshots = await queryUserSitesById(uid).get();
-        const siteRefs = siteSnapshots.docs.map(doc => doc.ref);
-        const siteIds = siteSnapshots.docs.map(doc => doc.id);
+        if (siteSnapshots.empty) return;
+        const siteDocs = siteSnapshots.docs;
+        const siteRefs = siteDocs.map(doc => doc.ref);
+        const siteIds = siteDocs.map(doc => doc.id);
+        const siteNameRefs = siteDocs.map(doc => {
+            const { name } = doc.data() as Site;
+            return USERS_COLLECTION.doc(uid).collection("sites").doc(name);
+        });
         return await Promise.all([
             deleteRefArray(siteRefs), // Delete all sites
+            deleteRefArray(siteNameRefs), // Delete all site name refs
             ...siteIds.map(id => deleteSitePagesById(id)), // And all pages of these sites
         ]);
     } catch (err) {
