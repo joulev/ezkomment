0d5798768b66161bdeff28f7930b3c8b2e0383b4
VietAnh1010 <vietanh101003@gmail.com>
2022-06-17 21:40:59 +0700

    Ensure uniqueness of names

--- server/utils/crud/pageUtils.ts
+++ server/utils/crud/pageUtils.ts
@@ -1,4 +1,10 @@
-import { FieldValue } from "firebase-admin/firestore";
+/**
+ * To ensure the uniqueness of page's name (regarding other pages that share the same site), we can
+ * use a sub-collection.
+ *
+ * Using sub-collection makes look up fast.
+ */
+import { WriteResult } from "firebase-admin/firestore";
 
 import { firestoreAdmin } from "~/server/firebase/firebaseAdmin";
 import CustomApiError from "~/server/utils/errors/customApiError";
@@ -26,11 +32,13 @@ export async function getPageById(pageId: string) {
 
 export async function createPage(data: CreatePageRequest) {
     try {
-        const { siteId, url } = data;
+        const { siteId, url, name } = data;
         const pageRef = PAGES_COLLECTION.doc();
         const pageId = pageRef.id;
+        const newPage = { id: pageId, ...data };
         return await firestoreAdmin.runTransaction(async t => {
             const siteRef = SITES_COLLECTION.doc(siteId);
+            const sitePageRef = siteRef.collection("pages").doc(name);
             const siteSnapshot = await siteRef.get();
             if (!siteSnapshot.exists) {
                 throw new CustomApiError("Site does not exist", 404);
@@ -39,7 +47,11 @@ export async function createPage(data: CreatePageRequest) {
             if (!url.includes(siteData.domain)) {
                 throw new CustomApiError("Site domain and page url do not match", 400);
             }
-            const newPage = { id: pageId, ...data };
+            /**
+             * If a page with the same name already exists in the site, this operation will fail,
+             * making the entire transaction fail as well.
+             */
+            t.create(sitePageRef, { id: pageId });
             t.create(pageRef, newPage);
             // statistic?
             // t.update(siteRef, { pageCount: FieldValue.increment(1) });
@@ -53,7 +65,25 @@ export async function createPage(data: CreatePageRequest) {
 export async function updatePageById(pageId: string, data: UpdatePageBodyParams) {
     try {
         const pageRef = PAGES_COLLECTION.doc(pageId);
-        return await pageRef.update(data);
+        const newName = data.name;
+        return await firestoreAdmin.runTransaction(async t => {
+            if (newName !== undefined) {
+                // We will need to look up to get the page's name
+                const pageSnapshot = await t.get(pageRef);
+                if (!pageSnapshot.exists) {
+                    throw new CustomApiError("Page does not exist", 404);
+                }
+                const pageData = pageSnapshot.data() as Page;
+                const oldName = pageData.name;
+                const siteId = pageData.siteId;
+                // Now update the name list
+                t.delete(SITES_COLLECTION.doc(siteId).collection("pages").doc(oldName));
+                t.create(SITES_COLLECTION.doc(siteId).collection("pages").doc(newName), {
+                    id: pageId,
+                });
+            }
+            t.update(pageRef, data);
+        });
     } catch (err) {
         handleFirestoreError(err);
     }
@@ -62,7 +92,15 @@ export async function updatePageById(pageId: string, data: UpdatePageBodyParams)
 export async function deletePageById(pageId: string) {
     try {
         const pageRef = PAGES_COLLECTION.doc(pageId);
-        return await pageRef.delete();
+        return await firestoreAdmin.runTransaction(async t => {
+            const pageSnapshot = await t.get(pageRef);
+            if (!pageSnapshot.exists) {
+                throw new CustomApiError("Page does not exist", 404);
+            }
+            const { name, siteId } = pageSnapshot.data() as Page;
+            t.delete(SITES_COLLECTION.doc(siteId).collection("pages").doc(name));
+            t.delete(pageRef);
+        });
     } catch (err) {
         handleFirestoreError(err);
     }
@@ -84,10 +122,17 @@ export async function listSitePagesById(siteId: string) {
 export async function deleteSitePagesById(siteId: string) {
     try {
         const pageSnapshots = await querySitePagesById(siteId).get();
-        const pageRefs = pageSnapshots.docs.map(doc => doc.ref);
-        const pageIds = pageSnapshots.docs.map(doc => doc.id);
+        if (pageSnapshots.empty) return;
+        const pageDocs = pageSnapshots.docs;
+        const pageRefs = pageDocs.map(doc => doc.ref);
+        const pageIds = pageDocs.map(doc => doc.id);
+        const pageNameRefs = pageDocs.map(doc => {
+            const { name } = doc.data() as Page;
+            return SITES_COLLECTION.doc(siteId).collection("pages").doc(name);
+        });
         return await Promise.all([
             deleteRefArray(pageRefs), // DELETE all pages
+            deleteRefArray(pageNameRefs), // DELETE all page name refs
             ...pageIds.map(id => deletePageCommentsById(id)), // And their comments
         ]);
     } catch (err) {
