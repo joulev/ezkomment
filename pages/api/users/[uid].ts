ffe2fc7e806f4bf773e2c809dc2355b6fc806329
VietAnh1010 <vietanh101003@gmail.com>
2022-06-17 22:37:55 +0700

    Added sanitizer for user update requests

--- pages/api/users/[uid].ts
+++ pages/api/users/[uid].ts
@@ -1,10 +1,11 @@
 import { deleteUser, getUser, updateUser } from "~/server/handlers/userHandlers";
 import { authenticateUidWithJWT } from "~/server/middlewares/authenticateRequests";
+import { sanitizeUpdateUserRequest } from "~/server/middlewares/sanitizeRequests/users";
 import { ncRouter } from "~/server/utils/nextHandlerUtils";
 
 const handler = ncRouter()
     .get(authenticateUidWithJWT, getUser)
-    .put(authenticateUidWithJWT, updateUser)
+    .put(authenticateUidWithJWT, sanitizeUpdateUserRequest, updateUser)
     .delete(authenticateUidWithJWT, deleteUser);
 
 export default handler;
