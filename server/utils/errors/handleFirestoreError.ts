a85e1f92abb925abb1a4df61c15fb819caaefbe4
VietAnh1010 <vietanh101003@gmail.com>
2022-06-18 22:34:02 +0700

    Handled code 6 of Firestore Error

--- server/utils/errors/handleFirestoreError.ts
+++ server/utils/errors/handleFirestoreError.ts
@@ -4,6 +4,8 @@ export function handleFirestoreError(err: unknown): never {
     if (err instanceof Error && !(err instanceof CustomApiError)) {
         const code: number = (err as any).code;
         if (code === 5) throw new CustomApiError(err, 404);
+        // Will change later, as code 6 may happen for different reason.
+        if (code === 6) throw new CustomApiError("Name has been taken", 409);
     }
     throw err;
 }
