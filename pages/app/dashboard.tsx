33191dcc97ea864857d93460c7a91a82d76a529e
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-19 15:55:11 +0700

    move empty state illustration to a separate component
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- pages/app/dashboard.tsx
+++ pages/app/dashboard.tsx
@@ -11,6 +11,7 @@ import useBreakpoint from "~/client/hooks/breakpoint";
 import { internalSWRGenerator } from "~/client/lib/fetcher";
 
 import A from "~/client/components/anchor";
+import BlankIllustration from "~/client/components/blankIllustration";
 import Button from "~/client/components/buttons";
 import Input from "~/client/components/forms/input";
 import Select from "~/client/components/forms/select";
@@ -36,46 +37,9 @@ const Loading: FC = () => (
 
 const EmptyState: FC = () => (
   <div className="flex flex-col md:flex-row items-center md:justify-center my-12 md:my-18 gap-12 md:gap-18">
-    <svg
-      className={clsx(
-        "text-neutral-300 dark:text-neutral-700 flex-shrink-0",
-        "w-[calc(286px/1.5)] sm:w-[256px] h-[calc(256px/1.5)] sm:h-[256px]"
-      )}
-      viewBox="0 0 286 256"
-    >
-      <path
-        d="M195.5 0.5C247.25 0.5 285.5 35.6553 285.5 75.3467C285.5 112.97 253.154 135.309 236.369 142.364C235.035 142.925 234.492 144.56 235.271 145.789L255.051 176.998C256.294 178.959 254.15 181.31 252.107 180.226L195.992 150.454C195.669 150.283 195.305 150.194 194.94 150.192C143.474 149.94 105.5 114.895 105.5 75.3467C105.5 35.6553 143.75 0.5 195.5 0.5Z"
-        className="stroke-card fill-card stroke-1"
-      />
-      <rect
-        width="90"
-        height="12"
-        rx="4"
-        transform="matrix(-1 0 0 1 240.5 48.5)"
-        className="fill-current"
-      />
-      <rect
-        width="90"
-        height="12"
-        rx="4"
-        transform="matrix(-1 0 0 1 240.5 69.5)"
-        className="fill-current"
-      />
-      <rect
-        width="90"
-        height="12"
-        rx="4"
-        transform="matrix(-1 0 0 1 240.5 90.5)"
-        className="fill-current"
-      />
-      <path
-        d="M90.5 75.5C38.75 75.5 0.5 110.655 0.5 150.347C0.5 187.97 32.8458 210.309 49.6308 217.364C50.9653 217.925 51.5077 219.56 50.7292 220.789L30.949 251.998C29.7059 253.959 31.8497 256.31 33.8929 255.226L90.0079 225.454C90.3313 225.283 90.6949 225.194 91.06 225.192C142.526 224.94 180.5 189.895 180.5 150.347C180.5 110.655 142.25 75.5 90.5 75.5Z"
-        className="stroke-card fill-card stroke-1"
-      />
-      <rect x="45.5" y="123.5" width="90" height="12" rx="4" className="fill-current" />
-      <rect x="45.5" y="144.5" width="90" height="12" rx="4" className="fill-current" />
-      <rect x="45.5" y="165.5" width="90" height="12" rx="4" className="fill-current" />
-    </svg>
+    <div className="flex-shrink-0 w-[calc(286px/1.5)] sm:w-[256px]">
+      <BlankIllustration />
+    </div>
     <div className="flex flex-col gap-9 justify-center">
       <div className="text-2xl sm:text-4xl text-center md:text-left">
         Add a new site to get&nbsp;started
