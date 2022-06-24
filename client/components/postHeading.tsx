540a34430cc87b86bc9e393b9e8a45d1dcb31ee2
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-24 10:00:42 +0700

    implement "updated" label for Markdown headings
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- client/components/postHeading.tsx
+++ client/components/postHeading.tsx
@@ -6,11 +6,24 @@ import A from "~/client/components/anchor";
 
 import { PostHeadingProps } from "~/types/client/components.type";
 
+const UpdatedLabel: FC = () => (
+  <span className="bg-indigo-500 text-white px-1.5 py-0.5 rounded text-sm font-normal inline-block relative ml-3 bottom-1">
+    updated
+  </span>
+);
+
 const PostHeading: FC<PostHeadingProps & { level: number }> = ({ level, id, children }) => {
   const HeadingTag: keyof JSX.IntrinsicElements = `h${level as 1 | 2 | 3 | 4 | 5 | 6}`;
   return (
     <HeadingTag id={id} className="relative group">
-      {children}
+      {typeof children === "string" && children.endsWith(" |updated|") ? (
+        <>
+          {children.substring(0, children.length - 10)}
+          <UpdatedLabel />
+        </>
+      ) : (
+        children
+      )}
       {level > 1 && (
         <A
           href={`#${id}`}
