1631e4ca761548ebf05e90b4c6bb2b34e34b4902
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-26 17:18:32 +0700

    minify the javascript sent to embed HTML
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- client/lib/generateCommentHTML.ts
+++ client/lib/generateCommentHTML.ts
@@ -1,91 +1,15 @@
 import { JSDOM } from "jsdom";
 
-// Probably will integrate this to the build chain and compress it.
-const extraScript = (apiURL: string) => `const apiURL = "${apiURL}";
-let hasFocus = false;
-let isVisible = false;
-let blockValidate = false;
-let COMMENTDIVCONTENT = "";
-
-function handler() {
-  validate();
-  blockValidate = true;
-  setTimeout(() => (blockValidate = false), 1000);
-}
-
-function sendFrameHeight() {
-  const height = Math.max(
-    document.body.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.clientHeight,
-    document.documentElement.scrollHeight,
-    document.documentElement.offsetHeight
-  );
-  window.parent.postMessage({ EzkFrameHeight: height }, "*");
-}
-
-function initialise() {
-  document.addEventListener("visibilitychange", () => {
-    isVisible = document.visibilityState === "visible";
-    if (hasFocus && isVisible && !blockValidate) handler();
-  });
-  window.addEventListener("focus", () => {
-    hasFocus = true;
-    if (hasFocus && isVisible && !blockValidate) handler();
-  });
-  window.addEventListener("message", event => {
-    if (event.data.hasOwnProperty("EzkFrameHeight")) sendFrameHeight();
-  });
-  document.querySelector("[data-ezk=form]").addEventListener("submit", onFormSubmit);
-}
-
-async function validate() {
-  const comments = await fetch(apiURL).then(res => res.json());
-  const commentsDiv = document.querySelector("[data-ezk=comments]");
-  if (COMMENTDIVCONTENT === "") COMMENTDIVCONTENT = commentsDiv.innerHTML;
-  commentsDiv.innerHTML = comments.length > 0
-    ? ""
-    : "<div>There are no comments yet. Be the first to join the conversation.</div>";
-  comments.forEach(({ author, text, date }) => {
-    const commentDocument = new DOMParser().parseFromString(COMMENTDIVCONTENT, "text/html");
-    const authorEl = commentDocument.querySelector("[data-ezk='comment-author']");
-    const contentEl = commentDocument.querySelector("[data-ezk='comment-content']");
-    const dateEl = commentDocument.querySelector("[data-ezk='comment-date']");
-    if (authorEl) authorEl.textContent = (author && author.length > 0) ? author : "Anonymous";
-    if (contentEl) contentEl.innerHTML = text; // already rendered safely on server side
-    if (dateEl) dateEl.textContent = date;
-    commentsDiv.innerHTML += commentDocument.body.innerHTML;
-  });
-  sendFrameHeight();
-}
-
-async function onFormSubmit(event) {
-  event.preventDefault();
-  const authorField = document.querySelector("[data-ezk='form-author']");
-  const commentField = document.querySelector("[data-ezk='form-content']");
-  const comment = { author: authorField.value, text: commentField.value };
-  authorField.value = "";
-  commentField.value = "";
-  await fetch(apiURL, {
-    method: "POST",
-    body: JSON.stringify(comment),
-    headers: { "Content-Type": "application/json" },
-  });
-  await validate();
-}
-
-window.addEventListener("load", () => {
-  initialise();
-  validate();
-});`;
-
 export default function generateCommentHTML(html: string, apiURL: string, isDark?: boolean) {
     if (typeof window !== "undefined") throw new Error("This function should be ran on the server");
     const dom = new JSDOM(html);
     const document = dom.window.document;
     const script = document.createElement("script");
-    script.textContent = extraScript(apiURL);
-    document.body.appendChild(script);
+    script.textContent = `import ezkomment from "/v1/js/${
+        process.env.NODE_ENV === "production" ? "ezkomment.min.js" : "ezkomment.js"
+    }";ezkomment("${apiURL}");`;
+    script.setAttribute("type", "module");
+    document.head.appendChild(script);
     if (isDark) document.documentElement.classList.add("dark");
     return dom.serialize();
 }
