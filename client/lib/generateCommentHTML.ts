6e9a526fae35e701691be3ef6041ce4eea56d81d
Vu Van Dung <joulev.vvd@yahoo.com>
2022-06-26 11:03:23 +0700

    update the sample <iframe>
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- client/lib/generateCommentHTML.ts
+++ client/lib/generateCommentHTML.ts
@@ -1,3 +1,4 @@
+import { JSDOM } from "jsdom";
 import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
@@ -5,59 +6,94 @@ import { unified } from "unified";
 
 import { PreviewComment } from "~/types/client/utils.type";
 
-export const comments = [
-    {
-        author: "John Doe",
-        text: "This is a great post. I like it.",
-        date: "3 months ago",
-    },
-    {
-        author: "Jane Doe",
-        text: "I think there is a mistake when you talk about CSS, but otherwise it is a great post.",
-        date: "1 year ago",
-    },
-];
+// Probably will integrate this to the build chain and compress it.
+const extraScript = (apiURL: string) => `const apiURL = "${apiURL}";
+let hasFocus = false;
+let isVisible = false;
+let blockValidate = false;
+let COMMENTDIVCONTENT = "";
 
-// https://stackoverflow.com/a/42308842
-const serverJs = `
-window.addEventListener('message', function (event) {
-    if (event.data == "FrameHeight") {
-        var body = document.body, html = document.documentElement;
-        var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-        event.source.postMessage({ "FrameHeight": height }, "*");
-    }
+function handler() {
+  validate();
+  blockValidate = true;
+  setTimeout(() => (blockValidate = false), 1000);
+}
+
+function sendFrameHeight() {
+  const height = Math.max(
+    document.body.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.clientHeight,
+    document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight
+  );
+  window.parent.postMessage({ EzkFrameHeight: height }, "*");
+}
+
+function initialise() {
+  document.addEventListener("visibilitychange", () => {
+    isVisible = document.visibilityState === "visible";
+    if (hasFocus && isVisible && !blockValidate) handler();
+  });
+  window.addEventListener("focus", () => {
+    hasFocus = true;
+    if (hasFocus && isVisible && !blockValidate) handler();
+  });
+  window.addEventListener("message", event => {
+    if (event.data.hasOwnProperty("EzkFrameHeight")) sendFrameHeight();
+  });
+  document.querySelector("[data-ezk=form]").addEventListener("submit", onFormSubmit);
+}
+
+async function validate() {
+  const comments = await fetch(apiURL).then(res => res.json());
+  const commentsDiv = document.querySelector("[data-ezk=comments]");
+  if (COMMENTDIVCONTENT === "") COMMENTDIVCONTENT = commentsDiv.innerHTML;
+  commentsDiv.innerHTML = comments.length > 0
+    ? ""
+    : "<div>There are no comments yet. Be the first to join the conversation.</div>";
+  comments.forEach(({ author, text, date }) => {
+    const commentDocument = new DOMParser().parseFromString(COMMENTDIVCONTENT, "text/html");
+    const authorEl = commentDocument.querySelector("[data-ezk='comment-author']");
+    const contentEl = commentDocument.querySelector("[data-ezk='comment-content']");
+    const dateEl = commentDocument.querySelector("[data-ezk='comment-date']");
+    if (authorEl) authorEl.textContent = (author && author.length > 0) ? author : "Anonymous";
+    if (contentEl) contentEl.innerHTML = text; // already rendered safely on server side
+    if (dateEl) dateEl.textContent = date;
+    commentsDiv.innerHTML += commentDocument.body.innerHTML;
+  });
+  sendFrameHeight();
+}
+
+async function onFormSubmit(event) {
+  event.preventDefault();
+  const authorField = document.querySelector("[data-ezk='form-author']");
+  const commentField = document.querySelector("[data-ezk='form-content']");
+  const comment = { author: authorField.value, text: commentField.value };
+  authorField.value = "";
+  commentField.value = "";
+  await fetch(apiURL, {
+    method: "POST",
+    body: JSON.stringify(comment),
+    headers: { "Content-Type": "application/json" },
+  });
+  await validate();
+}
+
+window.addEventListener("load", () => {
+  initialise();
+  validate();
 });`;
 
-/**
- * Construct a preview HTML for the given templates in the /site/[siteName]/customise page
- * @deprecated
- * @param allTemplate The template for the whole comment section, with a placeholder `<COMMENTS>`
- * to insert comments in
- * @param commentTemplate The template for each comment, with placeholders `<AUTHOR>`, `<TIME>` and
- * `<CONTENT>`
- * @param styles The CSS styles
- * @param addDarkClass Whether to add `.dark` to `<body>` or not
- * @returns The preview HTML
- */
-export default function generateCommentHTML(
-    allTemplate: string,
-    commentTemplate: string,
-    styles: string,
-    commentList?: typeof comments,
-    addDarkClass: boolean = false
-) {
-    const commentHTML = (commentList ?? comments).map(comment => {
-        const commentTemplateWithData = commentTemplate
-            .replace("<AUTHOR>", comment.author)
-            .replace("<TIME>", comment.date)
-            .replace("<CONTENT>", comment.text);
-        return commentTemplateWithData;
-    });
-    const allComments = commentHTML.join("");
-    const allTemplateWithData = allTemplate.replace("<COMMENTS>", allComments);
-    return `<html><head><style>${styles}</style></head><body${
-        addDarkClass ? ' class="dark"' : ""
-    }>${allTemplateWithData}<script>${serverJs}</script></body></html>`;
+export function generateCommentHTML(html: string, apiURL: string, isDark?: boolean) {
+    if (typeof window !== "undefined") throw new Error("This function should be ran on the server");
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+    const script = document.createElement("script");
+    script.textContent = extraScript(apiURL);
+    document.body.appendChild(script);
+    if (isDark) document.documentElement.classList.add("dark");
+    return dom.serialize();
 }
 
 export async function generatePreviewHTML(
