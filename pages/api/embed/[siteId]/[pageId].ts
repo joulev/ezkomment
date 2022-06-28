61712d2ec2c17458783bb4b459f01e6a02cb21ea
VietAnh1010 <vietanh101003@gmail.com>
2022-06-28 21:38:55 +0700

    Start new branch to embed comments

--- pages/api/embed/[siteId]/[pageId].ts
+++ pages/api/embed/[siteId]/[pageId].ts
@@ -5,6 +5,7 @@ import generateCommentHTML from "~/client/lib/generateCommentHTML";
 import html from "~/constants/sampleCommentCode";
 
 const handler: NextApiHandler = (_, res) => {
+    // Hmmm ... how should I implement this?
     const generatedHTML = generateCommentHTML(html, "/api/test");
     res.send(generatedHTML);
 };
