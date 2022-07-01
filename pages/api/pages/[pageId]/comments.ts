064715b08f4083ba5c1fed8ba592e84438ab77f5
VietAnh1010 <vietanh101003@gmail.com>
2022-07-01 22:43:50 +0700

    Compiled comments to html

--- pages/api/pages/[pageId]/comments.ts
+++ pages/api/pages/[pageId]/comments.ts
@@ -1,6 +1,10 @@
 import { listPageComments } from "~/server/handlers/pageHandlers";
 import { ncRouter } from "~/server/utils/nextHandlerUtils";
 
+/**
+ * TODO
+ * We will protect this route to only allow requests from "/api/embed/*"
+ */
 const handler = ncRouter().get(listPageComments);
 
 export default handler;
