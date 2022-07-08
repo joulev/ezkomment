1d83203efc3e225e8b131bfb450680bbc4b3be2e
Vu Van Dung <joulev.vvd@yahoo.com>
2022-07-08 16:58:22 +0700

    implement deleting comments
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- pages/app/site/[siteName]/[pageId]/index.tsx
+++ pages/app/site/[siteName]/[pageId]/index.tsx
@@ -1,25 +1,29 @@
 import clsx from "clsx";
 import { formatDistanceToNowStrict } from "date-fns";
 import { useRouter } from "next/router";
-import { FC, ReactNode, useState } from "react";
+import { FC, useState } from "react";
 
 import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
 import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
 import WebOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 
+import useAuth from "~/client/hooks/auth";
 import { usePage } from "~/client/hooks/page";
+import { UNABLE_TO_DELETE_COMMENT } from "~/client/lib/errors";
+import { internalFetcher } from "~/client/lib/fetcher";
 
 import A from "~/client/components/anchor";
 import pagePages from "~/client/components/app/handlePage";
+import AuthError from "~/client/components/auth/error";
 import BlankIllustration from "~/client/components/blankIllustration";
 import Button from "~/client/components/buttons";
 import CopiableCode from "~/client/components/copiableCode";
+import MsgBanner from "~/client/components/messageBanner";
 import Modal from "~/client/components/modal";
 import RightAligned from "~/client/components/utils/rightAligned";
 
-import { ClientPage, Comment } from "~/types/server";
-
-type CommentsProps = { comments: Comment[]; children?: ReactNode };
+import { ResponseMessage as Msg } from "~/types/client/utils.type";
+import { Comment } from "~/types/server";
 
 const Loading: FC = () => (
   <div className="mx-auto max-w-3xl">
@@ -39,38 +43,58 @@ const Loading: FC = () => (
   </div>
 );
 
-const Comments: FC<CommentsProps> = ({ comments, children }) => (
-  <div className={clsx("flex flex-col divide-y border rounded bg-card", "border-card divide-card")}>
-    {comments.map((comment, index) => (
-      <div key={index} className="p-6 flex flex-col gap-3 relative">
-        <div className="flex flex-col sm:flex-row gap-x-6">
-          <strong>{comment.author ?? "Anonymous"}</strong>
-          <time
-            className={clsx(
-              "text-muted sm:relative before:hidden sm:before:block",
-              "before:absolute before:content-['•'] before:-left-3 before:-translate-x-1/2"
-            )}
-            title={new Date(comment.date).toISOString()}
-          >
-            {formatDistanceToNowStrict(new Date(comment.date))} ago
-          </time>
-        </div>
-        {comment.text.length > 0 ? (
-          <div dangerouslySetInnerHTML={{ __html: comment.text }} className="post" />
-        ) : (
-          <div className="text-muted">(no comment body)</div>
-        )}
-        {children && <div className="absolute right-3 top-3">{children}</div>}
-      </div>
-    ))}
-  </div>
-);
-
-const PendingComments: FC<{ page: ClientPage; handleDelete: () => void }> = ({
-  page,
-  handleDelete,
+const CommentComponent: FC<{ comment: Comment; setMsg: (msg: Msg) => void }> = ({
+  comment,
+  setMsg,
 }) => {
+  const { setLoading } = useAuth();
+  const { mutate } = usePage();
+  const handleDelete = async () => {
+    setLoading(true);
+    try {
+      const { success } = await internalFetcher({
+        url: `/api/comments/${comment.id}`,
+        method: "DELETE",
+      });
+      if (!success) throw UNABLE_TO_DELETE_COMMENT;
+      await mutate();
+      setMsg({ type: "success", message: "Comment deleted successfully." });
+    } catch (err: any) {
+      setMsg({ type: "error", message: <AuthError err={err} /> });
+    }
+    setLoading(false);
+  };
+  return (
+    <div className="p-6 flex flex-col gap-3 relative">
+      <div className="flex flex-col sm:flex-row gap-x-6">
+        <strong>{comment.author ?? "Anonymous"}</strong>
+        <time
+          className={clsx(
+            "text-muted sm:relative before:hidden sm:before:block",
+            "before:absolute before:content-['•'] before:-left-3 before:-translate-x-1/2"
+          )}
+          title={new Date(comment.date).toISOString()}
+        >
+          {formatDistanceToNowStrict(new Date(comment.date))} ago
+        </time>
+      </div>
+      {comment.text.length > 0 ? (
+        <div dangerouslySetInnerHTML={{ __html: comment.text }} className="post" />
+      ) : (
+        <div className="text-muted">(no comment body)</div>
+      )}
+      <div className="absolute right-3 top-3 flex flex-row gap-3">
+        <Button icon={ClearOutlinedIcon} variant="tertiary" onClick={handleDelete} />
+      </div>
+    </div>
+  );
+};
+
+const PendingComments: FC = () => {
+  const { page } = usePage();
   const router = useRouter();
+  const [msg, setMsg] = useState<Msg>(null);
+  if (!page) return null;
   const pendingComments = page.comments.filter(c => c.status === "Pending");
   if (page.autoApprove)
     return (
@@ -92,21 +116,21 @@ const PendingComments: FC<{ page: ClientPage; handleDelete: () => void }> = ({
 
   return (
     <>
+      {msg && <MsgBanner msg={msg} />}
       <p>These comments require your approval before appearing in public.</p>
-      <Comments comments={pendingComments}>
-        <div className="flex flex-row gap-3">
-          <Button icon={ClearOutlinedIcon} variant="tertiary" onClick={handleDelete} />
-          <Button icon={CheckOutlinedIcon} />
-        </div>
-      </Comments>
+      <div className="flex flex-col divide-y border rounded bg-card border-card divide-card">
+        {pendingComments.map((comment, index) => (
+          <CommentComponent key={index} comment={comment} setMsg={setMsg} />
+        ))}
+      </div>
     </>
   );
 };
 
-const ApprovedComments: FC<{ page: ClientPage; handleDelete: () => void }> = ({
-  page,
-  handleDelete,
-}) => {
+const ApprovedComments: FC = () => {
+  const { page } = usePage();
+  const [msg, setMsg] = useState<Msg>(null);
+  if (!page) return null;
   const approvedComments = page.comments.filter(c => c.status === "Approved");
   if (approvedComments.length === 0)
     return (
@@ -119,13 +143,16 @@ const ApprovedComments: FC<{ page: ClientPage; handleDelete: () => void }> = ({
     );
   return (
     <>
+      {msg && <MsgBanner msg={msg} />}
       <p>
         These comments are now live and visible to all visitors of the page. However you can still
         delete any comments you want. Do note that deleted comments are irrecoverable.
       </p>
-      <Comments comments={approvedComments}>
-        <Button icon={ClearOutlinedIcon} variant="tertiary" onClick={handleDelete} />
-      </Comments>
+      <div className="flex flex-col divide-y border rounded bg-card border-card divide-card">
+        {approvedComments.map((comment, index) => (
+          <CommentComponent key={index} comment={comment} setMsg={setMsg} />
+        ))}
+      </div>
     </>
   );
 };
@@ -172,12 +199,12 @@ const Content: FC = () => {
       <hr />
       <section className="mb-9">
         <h2>Pending comments</h2>
-        <PendingComments page={page} handleDelete={handleDelete} />
+        <PendingComments />
       </section>
       <hr />
       <section>
         <h2>Approved comments</h2>
-        <ApprovedComments page={page} handleDelete={handleDelete} />
+        <ApprovedComments />
       </section>
       <Modal isVisible={showDeleteModal} onOutsideClick={() => setShowDeleteModal(false)}>
         <div className="p-6 max-w-lg">
