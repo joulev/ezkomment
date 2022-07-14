39827efe1b7f86f3e85b9ad697e60760c7feb668
VietAnh1010 <vietanh101003@gmail.com>
2022-07-14 21:24:05 +0700

    Added initial design for customisation
    I am tired, my head hurts

---
+++ server/utils/crud/customisationUtils.ts
@@ -0,0 +1,9 @@
+/**
+ * We will support `GET` and `PUT` requests for customisation files
+ * We will store the configuration in a subcollection?
+ */
+import { SITES_COLLECTION } from "~/server/firebase/firestoreCollections";
+
+export async function getSiteCustomisation(siteId: string) {}
+
+export async function updateSiteCustomisation(siteId: string) {}
