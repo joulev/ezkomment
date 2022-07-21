8959e5784600d0e0654e372cebce05004aae2276
Vu Van Dung <joulev.vvd@yahoo.com>
2022-07-21 08:58:35 +0700

    add horizontal ellipsis to preview string
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- pages/api/docs/search.ts
+++ pages/api/docs/search.ts
@@ -2,16 +2,12 @@ import docs from "~/docs/cache.json";
 
 import { ncRouter } from "~/server/utils/nextHandlerUtils";
 
-import { findMatchInDocContent } from "~/client/lib/docsSearch";
+import docsSearch from "~/client/lib/docsSearch";
 
 const handler = ncRouter().get((req, res) => {
     const query = req.query.query as string | undefined;
     if (!query) return res.status(400).json({ error: "Missing query" });
-    const words = query.split(" ");
-    const docsMatches = docs
-        .map(doc => findMatchInDocContent(doc, words))
-        .filter(({ matchCount }) => matchCount > 0);
-    res.status(200).json({ message: "Success", data: docsMatches });
+    res.status(200).json({ message: "Success", data: docsSearch(docs, query.split(" ")) });
 });
 
 export default handler;
