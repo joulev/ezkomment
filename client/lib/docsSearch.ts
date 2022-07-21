69583038068ab954da9409f5847fe5e2e4498110
Vu Van Dung <joulev.vvd@yahoo.com>
2022-07-21 10:40:31 +0700

    add tests for docsSearch
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- client/lib/docsSearch.ts
+++ client/lib/docsSearch.ts
@@ -4,15 +4,15 @@ import mergeRanges from "merge-ranges";
 
 import { DocsSearchData } from "~/types/client/utils.type";
 
-const config = {
-    previewLength: 100,
-    leftMargin: 20,
-};
-
 type DocCache = {
     source: string;
     title: string;
     content: string;
+};
+
+type Config = {
+    leftMargin: number;
+    previewLength: number;
 };
 
 /**
@@ -24,9 +24,9 @@ type DocCache = {
  * @param posArray In form of [[start1, end1], [start2, end2] ...]
  * @returns An array of objects that describes the preview display
  */
-function getHighlightedPreview(content: string, posArray: [number, number][]) {
+function getHighlightedPreview(content: string, posArray: [number, number][], config: Config) {
     const previewStartPos = Math.max(posArray[0][0] - config.leftMargin, 0);
-    const previewEndPos = previewStartPos + config.previewLength;
+    const previewEndPos = Math.min(previewStartPos + config.previewLength, content.length);
 
     let preview: { text: string; highlight: boolean }[] = [];
 
@@ -71,7 +71,11 @@ function getHighlightedPreview(content: string, posArray: [number, number][]) {
  * @param words The words to query
  * @returns Information about the search result
  */
-function findMatchInDocContent({ content, source, title }: DocCache, words: string[]) {
+function findMatchInDocContent(
+    { content, source, title }: DocCache,
+    words: string[],
+    config: Config
+) {
     const matches = words
         .map<[number, number][]>(word =>
             Array.from(content.matchAll(new RegExp(escapeStringRegexp(word), "gi"))).map(a => [
@@ -86,13 +90,20 @@ function findMatchInDocContent({ content, source, title }: DocCache, words: stri
         source,
         title,
         matchCount: matches.length,
-        preview: matches.length > 0 ? getHighlightedPreview(content, matchesMerged) : [],
+        preview: matches.length > 0 ? getHighlightedPreview(content, matchesMerged, config) : [],
     };
 }
 
-export default function docsSearch(docs: DocCache[], words: string[]): DocsSearchData {
+export default function docsSearch(
+    docs: DocCache[],
+    words: string[],
+    config: Config = {
+        previewLength: 100,
+        leftMargin: 20,
+    }
+): DocsSearchData {
     return docs
-        .map(doc => findMatchInDocContent(doc, words))
+        .map(doc => findMatchInDocContent(doc, words, config))
         .filter(({ matchCount }) => matchCount > 0)
         .sort((a, b) => b.matchCount - a.matchCount);
 }
