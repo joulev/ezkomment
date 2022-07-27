ad396832aa73df21831ef2bee37a91ea6929cb58
VietAnh1010 <vietanh101003@gmail.com>
2022-07-27 17:09:19 +0700

    Added endpoint to verify user's email

--- server/handlers/userHandlers.ts
+++ server/handlers/userHandlers.ts
@@ -1,14 +1,13 @@
 import { UpdateRequest } from "firebase-admin/auth";
-import { NextApiRequest } from "next";
 
 import * as UserUtils from "~/server/utils/crud/userUtils";
 import { deleteUserPhoto } from "~/server/utils/crud/imageUtils";
 import { extractFirstQueryValue } from "~/server/utils/nextHandlerUtils";
 
 import { ClientUser } from "~/types/server";
-import { ApiResponse } from "~/types/server/nextApi.type";
+import { ApiRequest, ApiResponse } from "~/types/server/nextApi.type";
 
-export async function getUser(req: NextApiRequest, res: ApiResponse) {
+export async function getUser(req: ApiRequest, res: ApiResponse) {
     const { uid } = extractFirstQueryValue(req);
     const user = await UserUtils.getUserById(uid);
     /**
@@ -18,14 +17,14 @@ export async function getUser(req: NextApiRequest, res: ApiResponse) {
     res.status(200).json({ message: "Got user's data", data: { ...user, sites } });
 }
 
-export async function updateUser(req: NextApiRequest, res: ApiResponse) {
+export async function updateUser(req: ApiRequest, res: ApiResponse) {
     const { uid } = extractFirstQueryValue(req);
     const data: UpdateRequest = req.body;
     await UserUtils.updateUserById(uid, data);
     res.status(200).json({ message: "Updated user" });
 }
 
-export async function deleteUser(req: NextApiRequest, res: ApiResponse) {
+export async function deleteUser(req: ApiRequest, res: ApiResponse) {
     const { uid } = extractFirstQueryValue(req);
     await UserUtils.deleteUserById(uid);
     // If the user exists, then
@@ -40,8 +39,18 @@ export async function deleteUser(req: NextApiRequest, res: ApiResponse) {
 // Interact with sites //
 /////////////////////////
 
-export async function listUserSites(req: NextApiRequest, res: ApiResponse) {
+export async function listUserSites(req: ApiRequest, res: ApiResponse) {
     const { uid } = extractFirstQueryValue(req);
     const data = await UserUtils.listUserSites(uid);
     res.status(200).json({ message: "Got user's sites", data });
+}
+
+///////////
+// Extra //
+///////////
+
+export async function verifyUserEmail(req: ApiRequest, res: ApiResponse) {
+    const { uid } = extractFirstQueryValue(req);
+    await UserUtils.updateUserById(uid, { emailVerified: true });
+    res.status(200).json({ message: "Verified user's email" });
 }
