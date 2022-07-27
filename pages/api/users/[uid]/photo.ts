a1ebdd1ced02ef2e128df598cc121be84e1bb9a3
VietAnh1010 <vietanh101003@gmail.com>
2022-07-27 17:09:19 +0700

    Added endpoint to verify user's email

--- pages/api/users/[uid]/photo.ts
+++ pages/api/users/[uid]/photo.ts
@@ -5,8 +5,6 @@ import { authenticatePathUidWithJWT } from "~/server/middlewares/authenticateReq
 import { parseUserPhoto } from "~/server/middlewares/parseForms";
 import { ncRouter } from "~/server/utils/nextHandlerUtils";
 
-import { ApiRequestWithFormData } from "~/types/server/nextApi.type";
-
 export const config: PageConfig = {
     api: {
         bodyParser: false,
