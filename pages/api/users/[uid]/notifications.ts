921e86e101d1432c7f521793cb4cebe215603e04
VietAnh1010 <vietanh101003@gmail.com>
2022-07-29 23:30:12 +0700

    Implemented listUserNotifications

--- pages/api/users/[uid]/notifications.ts
+++ pages/api/users/[uid]/notifications.ts
@@ -1,7 +1,7 @@
-import { initializeUser } from "~/server/handlers/userHandlers";
+import { listUserNotifications } from "~/server/handlers/userHandlers";
 import { authenticatePathUidWithJWT } from "~/server/middlewares/authenticateRequests";
 import { ncRouter } from "~/server/utils/nextHandlerUtils";
 
-const handler = ncRouter().post(authenticatePathUidWithJWT, initializeUser);
+const handler = ncRouter().get(authenticatePathUidWithJWT, listUserNotifications);
 
 export default handler;
