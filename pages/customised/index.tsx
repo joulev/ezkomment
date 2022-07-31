1d1aad3b78806a61caba498254bc50c52ea54783
Vu Van Dung <joulev.vvd@yahoo.com>
2022-07-31 17:11:34 +0700

    add drafts for customised and with-api demos
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

---
+++ pages/customised/index.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import { NextPage } from "next";
+
+import Article from "~/components/article";
+import Comment from "~/components/comment";
+import { Author } from "~/components/variations/default";
+
+const Page: NextPage = () => (
+  <div className="bg-neutral-100 min-h-screen">
+    <div className="py-24 px-8">
+      <div
+        className={clsx(
+          "w-full max-w-prose mx-auto prose dark:prose-invert prose-neutral text-base",
+          "font-['Supreme'] prose-h1:font-['Chubbo']"
+        )}
+      >
+        <Article authorElement={Author} />
+        <section>
+          <h2>Comments</h2>
+          {/* <Comment ezkUrl="https://ezkomment.joulev.dev/embed/EtWkLUcKPYrz5k5ynb16/4JJxJZE1pdqBgTOOrP79" /> */}
+        </section>
+      </div>
+    </div>
+  </div>
+);
+
+export default Page;
---
+++ pages/with-api/index.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from "next";
+
+import Article from "~/components/article";
+import { Author } from "~/components/variations/default";
+
+const Page: NextPage = () => (
+  <div className="bg-neutral-100 min-h-screen">
+    <div className="py-24 px-8">
+      <div className="w-full max-w-prose mx-auto prose dark:prose-invert prose-neutral text-base">
+        <Article authorElement={Author} />
+        <section>
+          <h2>Comments</h2>
+          {/* <Comment ezkUrl="https://ezkomment.joulev.dev/embed/EtWkLUcKPYrz5k5ynb16/4JJxJZE1pdqBgTOOrP79" /> */}
+        </section>
+      </div>
+    </div>
+  </div>
+);
+
+export default Page;
