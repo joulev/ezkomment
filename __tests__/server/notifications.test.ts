460a4c3a82edfb15dd1cf0e2edb41faa67fd7ca3
Vu Van Dung <joulev.vvd@yahoo.com>
2022-08-07 00:30:36 +0800

    fix backend tests failures
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>
    Co-authored-by: VietAnh1010 <vietanh101003@gmail.com>

--- __tests__/server/notifications.test.ts
+++ __tests__/server/notifications.test.ts
@@ -3,20 +3,24 @@ import * as NotificationUtils from "~/server/utils/crud/notificationUtils";
 import * as TestUtils from "~/server/utils/testUtils";
 import { deleteUserSites } from "~/server/utils/crud/userUtils";
 
+import { NewCommentNotification } from "~/types/server";
+
 const { nonExistingPageId } = testOnly.nonExistingIds;
 
 describe("Test notification utils", () => {
     const uid = TestUtils.randomUUID();
     const siteId = TestUtils.randomUUID();
-    const pageId = TestUtils.randomUUID();
+    const pageId1 = TestUtils.randomUUID();
+    const pageId2 = TestUtils.randomUUID();
 
     const mainSite = TestUtils.createTestSite({ uid, id: siteId, pageCount: 1 });
-    const mainPage = TestUtils.createTestPage({ uid, siteId, id: pageId });
+    const mainPage1 = TestUtils.createTestPage({ uid, siteId, id: pageId1 });
+    const mainPage2 = TestUtils.createTestPage({ uid, siteId, id: pageId2 });
 
     beforeAll(async () => {
         TestUtils.importFirestoreEntities({
             sites: [mainSite],
-            pages: [mainPage],
+            pages: [mainPage1, mainPage2],
         });
     });
 
@@ -36,30 +40,40 @@ describe("Test notification utils", () => {
 
     it("Should create new notification correctly", async () => {
         await CommentUtils.createComment({
-            pageId,
+            pageId: pageId1,
             author: "Seiran",
             text: "The Rabbit Has Landed",
         });
         let notifications = await NotificationUtils.listUserNotifications(uid);
         expect(notifications).toHaveLength(1);
         expect(notifications[0]).toMatchObject({
-            id: pageId,
+            id: pageId1,
             authors: ["Seiran"],
             siteName: mainSite.name,
-            pageTitle: mainPage.title,
+            pageTitle: mainPage1.title,
         });
         await CommentUtils.createComment({
-            pageId,
+            pageId: pageId1,
             author: "Seiran",
             text: "Unforgettable, the Nostalgic Greenery",
         });
         notifications = await NotificationUtils.listUserNotifications(uid);
         expect(notifications).toHaveLength(1);
+        expect((notifications[0] as NewCommentNotification).authors).toHaveLength(1);
         await CommentUtils.createComment({
-            pageId,
+            pageId: pageId1,
             author: "Kaguya Houraisan",
             text: "Flight of the Bamboo Cutter ~ Lunatic Princess",
         });
+        notifications = await NotificationUtils.listUserNotifications(uid);
+        expect(notifications).toHaveLength(1);
+        expect((notifications[0] as NewCommentNotification).authors).toHaveLength(2);
+        await CommentUtils.createComment({
+            pageId: pageId2,
+            author: "Eirin Yagokoro",
+            text: "Gensoko Millenium ~ History of the Moon",
+        });
+        notifications = await NotificationUtils.listUserNotifications(uid);
         expect(notifications).toHaveLength(2);
     });
 
