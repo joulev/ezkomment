44fa7a75ba5a20c0efb3f65d837939c16df27d9e
Vu Van Dung <joulev.vvd@yahoo.com>
2022-08-07 00:14:20 +0800

    fix typescript build error
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- client/context/auth.ts
+++ client/context/auth.ts
@@ -5,6 +5,8 @@ import { AppAuth } from "~/types/client/auth.type";
 const AuthContext = createContext<AppAuth>({
     user: undefined,
     mutate: async () => undefined,
+    notifications: undefined,
+    mutateNotifications: async () => undefined,
     loading: true,
     setLoading: () => undefined,
 });
