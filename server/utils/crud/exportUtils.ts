adab53cc184b9609c9d49d36a8d7fdc00779050b
VietAnh1010 <vietanh101003@gmail.com>
2022-08-07 00:44:31 +0800

    Initial implementation for exporting pages and sites

--- server/utils/crud/exportUtils.ts
+++ server/utils/crud/exportUtils.ts
@@ -1,7 +1,22 @@
 import { ExportPage, ExportSite, ExportUser } from "~/types/server";
 
+import { getSiteCustomisation } from "./customisationUtils";
 import { getClientPageWithUid } from "./pageUtils";
+import { getClientSiteWithUid } from "./siteUtils";
 
 export async function exportPageWithUid(uid: string, pageId: string): Promise<ExportPage> {
     return await getClientPageWithUid(uid, pageId);
+}
+
+export async function exportSiteWithUid(uid: string, siteId: string): Promise<ExportSite> {
+    const { pages: rawPages, ...rest } = await getClientSiteWithUid(uid, siteId);
+    const [{ customisation }, ...pages] = await Promise.all([
+        getSiteCustomisation(siteId),
+        ...rawPages.map(page => exportPageWithUid(uid, page.id)),
+    ]);
+    return { ...rest, pages, customisation };
+}
+
+export async function exportUserById(uid: string): Promise<ExportUser | undefined> {
+    return undefined;
 }
