4f2ea480672873fbe6ee3ff6d58e854198ac9b60
Vu Van Dung <joulev.vvd@yahoo.com>
2022-08-10 21:36:01 +0800

    start working on splashdown documents
    Signed-off-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- next.config.mjs
+++ next.config.mjs
@@ -18,9 +18,44 @@ const nextConfig = {
       permanent: true,
     },
     {
+      source: "/docs/getting-started",
+      destination: "/docs/tutorial/getting-started",
+      permanent: true,
+    },
+    {
       source: "/app", // likely will change this when authentication is added
       destination: "/app/dashboard",
       permanent: true,
+    },
+    {
+      source: "/orbital/liftoff-video",
+      destination: "https://drive.google.com/file/d/1wkvvl-EeYFRbqdmqBBfFu5B-NO_vVxFK/view",
+      permanent: false,
+    },
+    {
+      source: "/orbital/ms1-video",
+      destination: "https://drive.google.com/file/d/1TE0dBZ3PkU9FSMSvTEMnb3dLs9U4CUyl/view",
+      permanent: false,
+    },
+    {
+      source: "/orbital/ms2-video",
+      destination: "https://drive.google.com/file/d/1gyDeqEbk3Mu6ZZqLgoa06pH4UgrewekD/view",
+      permanent: false,
+    },
+    {
+      source: "/orbital/ms3-video",
+      destination: "https://drive.google.com/file/d/1yAn4dCjKpKoHwNMfsfeS3Mu3pzJZYzgZ/view",
+      permanent: false,
+    },
+    {
+      source: "/orbital/splashdown-video",
+      destination: "https://example.com",
+      permanent: false,
+    },
+    {
+      source: "/orbital/splashdown-source",
+      destination: "https://example.com",
+      permanent: false,
     },
   ],
   rewrites: async () => [
