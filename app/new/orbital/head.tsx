bb77fc1c039dc19f57017b4610031d782f12fb24
Vu Van Dung <me@joulev.dev>
2022-11-04 18:36:27 +0800

    port /orbital to next13's appDir
    Signed-off-by: Vu Van Dung <me@joulev.dev>

---
+++ app/new/orbital/head.tsx
@@ -0,0 +1,16 @@
+import getOgImage from "~/client/lib/getOgImage";
+import Seo from "~/client13/components/seo";
+
+import { SeoProps } from "~/types/client/components.type";
+
+const seo: SeoProps = {
+  title: "Orbital | ezkomment",
+  description:
+    "ezkomment is an NUS Orbital project. This page includes all public information of ezkomment that is related to NUS Orbital 2022.",
+  image: getOgImage({ title: "ezkomment @ NUS Orbital", label: "orbital" }),
+  url: "https://ezkomment.joulev.dev/orbital",
+};
+
+export default function Head() {
+  return <Seo {...seo} />;
+}
