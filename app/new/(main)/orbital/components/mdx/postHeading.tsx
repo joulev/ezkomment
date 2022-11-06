77d32c6ede030e29861432d49d466b136e563e94
Vu Van Dung <me@joulev.dev>
2022-11-06 23:45:21 +0800

    remove the (public) group for orbital pages
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/new/(main)/orbital/components/mdx/postHeading.tsx
+++ app/new/(main)/orbital/components/mdx/postHeading.tsx
@@ -6,7 +6,12 @@ import { PostHeadingProps } from "~/types/client/components.type";
 export default function PostHeading({ level, id, children }: PostHeadingProps & { level: number }) {
   const HeadingTag: keyof JSX.IntrinsicElements = `h${level as 1 | 2 | 3 | 4 | 5 | 6}`;
   return (
-    <HeadingTag id={id} className="relative group">
+    <HeadingTag id={id} className="flex flex-row items-baseline gap-1.5 group -ml-6">
+      {level > 1 && (
+        <A href={`#${id}`} className="opacity-0 group-hover:opacity-100 transition">
+          <Hash size={18} />
+        </A>
+      )}
       {typeof children === "string" && children.endsWith(" (updated)") ? (
         <>
           {children.substring(0, children.length - 10)}
@@ -16,14 +21,6 @@ export default function PostHeading({ level, id, children }: PostHeadingProps & 
         </>
       ) : (
         children
-      )}
-      {level > 1 && (
-        <A
-          href={`#${id}`}
-          className="absolute bottom-0 right-full pr-1 opacity-0 group-hover:opacity-100 transition"
-        >
-          <Hash />
-        </A>
       )}
     </HeadingTag>
   );
