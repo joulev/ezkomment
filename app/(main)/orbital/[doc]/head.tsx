fdbbc6b61b1a5a612dc3f7352f90489fe88ced64
Vu Van Dung <me@joulev.dev>
2022-11-07 15:10:30 +0800

    finally fix the weird vercel build error (I hope)
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/(main)/orbital/[doc]/head.tsx
+++ app/(main)/orbital/[doc]/head.tsx
@@ -3,7 +3,10 @@ import { Params } from "./page";
 // for some reasons next-mdx-remote doesn't work in head.tsx so I have to use one extra dependency
 import matter from "gray-matter";
 
+const docs = ["proposal", "ms1-readme", "ms2-readme", "ms3-readme"];
+
 export default async function OrbitalHead({ params: { doc } }: { params: Params }) {
+  if (!doc || !docs.includes(doc)) return null;
   const raw = await import(`./${doc}.md`).then(m => m.default);
   const {
     data: { title, seoTitle, seoDescription: description, seoUrl },
