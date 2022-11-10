45ec0e275149560de1d9fc78ca952d943edcbda0
Vu Van Dung <me@joulev.dev>
2022-11-10 20:59:15 +0800

    start working on new auth system for RSC
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/(auth)/components/provider.client.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import AuthContext from "~/app/context/auth";
-import { useAuthInit } from "~/app/hooks/auth";
-
-export default function AuthProvider({ children }: React.PropsWithChildren) {
-  const providedAuth = useAuthInit();
-  return <AuthContext.Provider value={providedAuth}>{children}</AuthContext.Provider>;
-}
