b9ce781e4c28f84c42376258d36589f8557edf2b
Vu Van Dung <me@joulev.dev>
2022-11-12 01:41:43 +0800

    add head tag for dashboard
    Signed-off-by: Vu Van Dung <me@joulev.dev>

---
+++ app/(auth)/app/dashboard/head.tsx
@@ -0,0 +1,8 @@
+export default function AppDashboardHead() {
+  return (
+    <>
+      <title>Dashboard | ezkomment</title>
+      <meta name="robots" content="noindex" />
+    </>
+  );
+}
--- app/(auth)/app/dashboard/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function AppDashboardLoading() {
-  return <div>Loading</div>;
-}
