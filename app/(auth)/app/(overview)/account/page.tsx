a7d5d236c279c5fadde5ecbee5625795015dab1e
Vu Van Dung <me@joulev.dev>
2022-11-13 02:18:51 +0800

    do not support linking account now
    Rationale: mongodb doesnt support it why should we /s
    
    but seriously I don't see much value in that, and there's little
    documentation on how to do so with sessionCookie
    
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/(auth)/app/(overview)/account/page.tsx
+++ app/(auth)/app/(overview)/account/page.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import { useState } from "react";
-import { AlertTriangle, HardDrive, User, Save, Trash } from "lucide-react";
+import { AlertTriangle, HardDrive, User, Save } from "lucide-react";
 import { internalPut, internalPutNotJson } from "~/app/(auth)/internal-fetch";
-import { useBreakpoint } from "~/app/breakpoint";
 import { useLoadingState } from "~/app/loading-state";
 import { useSetToast } from "~/app/(auth)/toast";
 import { useAuth } from "~/app/(auth)/app/user";
 import AuthError from "~/app/components/auth-error";
 import Banner from "~/app/components/banner";
 import Button from "~/app/components/buttons.client";
-import CopiableCode from "~/app/components/copiable-code.client";
 import IconUpload from "~/app/components/forms/icon-upload.client";
 import InputDetachedLabel from "~/app/components/forms/input-detached-label";
 import Modal from "~/app/components/modal.client";
-import GitHubLogo from "~/app/(auth)/components/github-logo";
-import GoogleLogo from "~/app/(auth)/components/google-logo";
 import RightAligned from "~/app/components/utils/right-aligned";
 
 function ProfileSection() {
@@ -101,55 +97,6 @@ function ProfileSection() {
   );
 }
 
-function LinkAccountSection() {
-  const {
-    user: { providerData },
-  } = useAuth();
-  const breakpoint = useBreakpoint();
-
-  return (
-    <section>
-      <h2>Link accounts</h2>
-      <p>
-        Your account is currently linked with the following services, and you can use these to sign
-        in to your account. You must link your account with at least one service.
-      </p>
-      <div className="flex flex-col mb-6 bg-card rounded overflow-hidden border border-card divide-y divide-card">
-        {providerData.map(data => (
-          <div className="flex flex-row p-3 pl-4.5 gap-x-4.5 items-center" key={data.providerId}>
-            {data.providerId === "github.com" && <GitHubLogo size={36} />}
-            {data.providerId === "google.com" && <GoogleLogo size={36} />}
-            <div className="min-w-0 flex-grow">
-              <div className="font-medium truncate">{data.displayName ?? "No username"}</div>
-              <div className="text-xs text-muted truncate">{data.email ?? "No email"}</div>
-            </div>
-            {providerData.length > 1 && (
-              <Button variant="danger" icon={Trash}>
-                {["xs", "md"].includes(breakpoint) ? null : "Unlink"}
-              </Button>
-            )}
-          </div>
-        ))}
-      </div>
-      <div className="grid grid-cols-2 gap-x-6 gap-y-3">
-        <span className="col-span-2 font-semibold">Link new account</span>
-        <Button
-          variant="tertiary"
-          disabled={!!providerData.find(data => data.providerId === "github.com")}
-        >
-          GitHub
-        </Button>
-        <Button
-          variant="tertiary"
-          disabled={!!providerData.find(data => data.providerId === "google.com")}
-        >
-          Google
-        </Button>
-      </div>
-    </section>
-  );
-}
-
 function ExportDataSection() {
   return (
     <section>
@@ -205,26 +152,13 @@ function DeleteAccountSection() {
 }
 
 export default function AppAccountPage() {
-  const { user } = useAuth();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12">
       <div>
         <ProfileSection />
-        <hr />
-        <section>
-          <h2>User ID</h2>
-          <p>Your user ID is</p>
-          <CopiableCode content={user.uid} className="mb-6" />
-          <p>
-            Please use this ID to identify yourself if you need to contact us for support for issues
-            related to your profile.
-          </p>
-        </section>
         <hr className="md:hidden" />
       </div>
       <div>
-        <LinkAccountSection />
-        <hr />
         <ExportDataSection />
         <hr />
         <DeleteAccountSection />
