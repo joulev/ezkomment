c5ea6a2cfe9d51bfd157e6ebc708e2e063138d51
Vu Van Dung <me@joulev.dev>
2022-11-14 03:05:40 +0800

    britishify some american english words
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- server/errors/auth-error.ts
+++ server/errors/auth-error.ts
@@ -4,7 +4,7 @@ export function handleUserError(err: unknown): never {
     if (err instanceof Error && !(err instanceof CustomApiError)) {
         const code: string = (err as any).errorInfo?.code ?? "";
         if (code === "auth/user-not-found") throw new CustomApiError(err, 404);
-        // should not happen as request body is sanitized
+        // should not happen as request body is sanitised
         if (code.startsWith("auth/invalid")) throw new CustomApiError(err, 400);
     }
     throw err;
