b14494b622f5375bb2e8d86d476276f2ec7e637e
Vu Van Dung <me@joulev.dev>
2022-11-14 03:09:13 +0800

    naming consistency: rename post() to create()
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- pages/api/sites/index.ts
+++ pages/api/sites/index.ts
@@ -1,9 +1,9 @@
-import { post } from "~/server/handlers/site";
+import { create } from "~/server/handlers/site";
 import authenticate from "~/server/middlewares/authenticate";
 import { create as sanitiseCreate } from "~/server/middlewares/sanitisers/site";
 import { createHandler, createRouter } from "~/server/next-connect";
 
 const router = createRouter();
-router.post(authenticate, sanitiseCreate, post);
+router.post(authenticate, sanitiseCreate, create);
 
 export default createHandler(router);
--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,9 +1,9 @@
-import { get, post, update, remove } from "~/server/handlers/user";
+import { get, create, update, remove } from "~/server/handlers/user";
 import authenticate from "~/server/middlewares/authenticate";
 import { update as sanitiseUpdate } from "~/server/middlewares/sanitisers/user";
 import { createHandler, createRouter } from "~/server/next-connect";
 
 const router = createRouter();
-router.use(authenticate).get(get).post(post).put(sanitiseUpdate, update).delete(remove);
+router.use(authenticate).get(get).post(create).put(sanitiseUpdate, update).delete(remove);
 
 export default createHandler(router);
