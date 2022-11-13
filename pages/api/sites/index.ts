fb632fb32798af4c7ddbebd19d1b23a444a4a821
Vu Van Dung <me@joulev.dev>
2022-11-14 03:07:33 +0800

    rename sanitisers users->user, sites->site for consistency
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- pages/api/sites/index.ts
+++ pages/api/sites/index.ts
@@ -1,6 +1,6 @@
 import { post } from "~/server/handlers/site";
 import authenticate from "~/server/middlewares/authenticate";
-import { create as sanitiseCreate } from "~/server/middlewares/sanitisers/sites";
+import { create as sanitiseCreate } from "~/server/middlewares/sanitisers/site";
 import { createHandler, createRouter } from "~/server/next-connect";
 
 const router = createRouter();
--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,6 +1,6 @@
 import { get, post, update, remove } from "~/server/handlers/user";
 import authenticate from "~/server/middlewares/authenticate";
-import { update as sanitiseUpdate } from "~/server/middlewares/sanitisers/users";
+import { update as sanitiseUpdate } from "~/server/middlewares/sanitisers/user";
 import { createHandler, createRouter } from "~/server/next-connect";
 
 const router = createRouter();
